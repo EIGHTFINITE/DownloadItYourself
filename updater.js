e0b73ce22a11749497e441d77e05d8f96fd4f673
Kevin van Rijn <KIPdeKIP@users.noreply.github.com>
2018-02-22 17:36:12 +0100

    Commit base script

--- updater.js
+++ updater.js
@@ -1,1 +1,18 @@
-//
+var fs = require('fs');
+var url = require('url');
+var request = require('request');
+var cheerio = require('cheerio');
+
+fs.readFile("downloadlist.json", "utf8", function(err, data) {
+    if (err) throw err;
+    var obj = JSON.parse(data);
+    var temp = {};
+    var i, current, $;
+    for (i = 0; i < obj.downloads.length; i++) {
+        current = obj.downloads[i];
+		fs.readFile("drivers/" + url.parse(current.url).host + ".js", "utf8", function(err, script) {
+			if (err) throw err;
+			eval(script); // There must be a better way to do this.
+		});
+    }
+});
