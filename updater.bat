ae581be42b97aba18872e3cd42927f114d24d53f
Kevin van Rijn <KIPdeKIP@users.noreply.github.com>
2018-02-22 15:14:45 +0100

    Add Windows launcher

---
+++ updater.bat
@@ -0,0 +1,7 @@
+@cls
+@echo off
+setlocal
+cd /d %~dp0
+reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > nul && set osArchitecture=86 || set osArchitecture=64
+cmd /c bin\windows\x%osArchitecture%\node\npm install 1>nul 2>nul
+bin\windows\x%osArchitecture%\node\node updater.js
