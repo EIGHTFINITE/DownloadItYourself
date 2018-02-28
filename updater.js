5fac080d41ff24f59b79ec63fb22838bb27d7533
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2018-02-28 13:26:51 +0100

    Use double quotes

--- updater.js
+++ updater.js
@@ -1,7 +1,7 @@
-var fs = require('fs-extra');
-var url = require('url');
-var request = require('request');
-var cheerio = require('cheerio');
+var fs = require("fs-extra");
+var url = require("url");
+var request = require("request");
+var cheerio = require("cheerio");
 
 fs.readFile("downloadlist.json", "utf8", function(err, data) {
     if (err) throw err;
