b1aed2d56320df416fbc618034a5a87563b487f6
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2018-02-28 13:26:44 +0100

    Move picking the driver into a new function

--- updater.js
+++ updater.js
@@ -8,13 +8,16 @@ fs.readFile("downloadlist.json", "utf8", function(err, data) {
     var obj = JSON.parse(data);
     var dir = obj.config.folder;
     fs.ensureDirSync(dir);
-    var temp = {};
-    var i, current, $;
     for (i = 0; i < obj.downloads.length; i++) {
-        current = obj.downloads[i];
-        fs.readFile("drivers/" + url.parse(current.url).host + ".js", "utf8", function(err, script) {
-            if (err) throw err;
-            eval(script); // There must be a better way to do this.
-        });
+        parseDownload(obj, obj.downloads[i]);
     }
 });
+
+function parseDownload(obj, current) {
+    var temp = {};
+    var $;
+    fs.readFile("drivers/" + url.parse(current.url).host + ".js", "utf8", function(err, script) {
+        if (err) throw err;
+        eval(script); // There must be a better way to do this.
+    });
+}
