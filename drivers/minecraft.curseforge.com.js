a2828a642c191a0bdd0678305baeee503e2372dd
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2018-02-28 16:59:28 +0100

    Log what thread is communicating

--- drivers/minecraft.curseforge.com.js
+++ drivers/minecraft.curseforge.com.js
@@ -2,7 +2,7 @@
  * Driver for downloading from minecraft.curseforge.com
  */
 temp.href = current.url + "/files?" + (obj.config["curseforge-version"] ? "filter-game-version=" + obj.config["curseforge-version"] : "");
-console.log("Navigating to: " + temp.href);
+console.log("[" + i + "] Navigating to: " + temp.href);
 request(temp.href, function(err, response, html) {
     if (err) throw err;
     $ = cheerio.load(html);
@@ -10,7 +10,7 @@ request(temp.href, function(err, response, html) {
     temp.host = response.request.uri.host;
     temp.path = $("a.overflow-tip.twitch-link").attr('href');
     temp.href = temp.protocol + "//" + temp.host + temp.path;
-    console.log("Navigating to: " + temp.href);
+    console.log("[" + i + "] Navigating to: " + temp.href);
     request(temp.href, function(err, response, html) {
         if (err) throw err;
         $ = cheerio.load(html);
@@ -19,7 +19,7 @@ request(temp.href, function(err, response, html) {
         temp.path = $("a.button.fa-icon-download:not(.alt)").attr('href');
         temp.href = temp.protocol + "//" + temp.host + temp.path;
         temp.file = $("div.info-data.overflow-tip").text().trim();
-        console.log("Downloading: " + temp.href + " as " + temp.file);
+        console.log("[" + i + "] Downloading: " + temp.href + ' as "' + temp.file + '"');
         request(temp.href).pipe(fs.createWriteStream(obj.config.folder + "/" + temp.file));
     });
 });
