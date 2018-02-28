94489d823993545f17a8f6ed76fa6a81f13ef5a8
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2018-02-28 15:34:17 +0100

    Enable strict mode

--- updater.bat
+++ updater.bat
@@ -4,4 +4,4 @@ setlocal
 cd /d %~dp0
 reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > nul && set osArchitecture=86 || set osArchitecture=64
 cmd /c bin\windows\x%osArchitecture%\node\npm install 1>nul 2>nul
-bin\windows\x%osArchitecture%\node\node updater.js
+bin\windows\x%osArchitecture%\node\node --use_strict updater.js
