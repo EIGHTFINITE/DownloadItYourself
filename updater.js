8fde9f991c95934d60fe3bd4e7d63d62fd0c4a6b
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2018-02-28 12:10:10 +0100

    Write to mods folder

--- updater.js
+++ updater.js
@@ -1,4 +1,4 @@
-var fs = require('fs');
+var fs = require('fs-extra');
 var url = require('url');
 var request = require('request');
 var cheerio = require('cheerio');
@@ -6,13 +6,15 @@ var cheerio = require('cheerio');
 fs.readFile("downloadlist.json", "utf8", function(err, data) {
     if (err) throw err;
     var obj = JSON.parse(data);
+    var dir = obj.config.folder;
+    fs.ensureDirSync(dir);
     var temp = {};
     var i, current, $;
     for (i = 0; i < obj.downloads.length; i++) {
         current = obj.downloads[i];
-		fs.readFile("drivers/" + url.parse(current.url).host + ".js", "utf8", function(err, script) {
-			if (err) throw err;
-			eval(script); // There must be a better way to do this.
-		});
+        fs.readFile("drivers/" + url.parse(current.url).host + ".js", "utf8", function(err, script) {
+            if (err) throw err;
+            eval(script); // There must be a better way to do this.
+        });
     }
 });
