152af6fdba5aa443253a987e9aae049c04d372dd
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2018-03-06 12:03:00 +0100

    Ensure file thread numbers are all the same length

--- updater.js
+++ updater.js
@@ -12,40 +12,40 @@ fs.readFile("downloadlist.json", "utf8", function(err, data) {
     obj = JSON.parse(data);
     fs.ensureDirSync(obj.config.folder);
     for (var i = 0; i < obj.downloads.length; i++) {
-        checkFile(obj, obj.downloads[i], i);
-        parseDownload(obj, obj.downloads[i], i);
+        checkFile(obj, obj.downloads[i], i, i.toString().padStart(obj.downloads.length.toString().length, "0"));
+        parseDownload(obj, obj.downloads[i], i, i.toString().padStart(obj.downloads.length.toString().length, "0"));
     }
 });
 
-function checkFile(obj, current, i) {
-    console.log("[" + i + "] Checking " + (current.name ? current.name : current.url) + " file integrity.");
+function checkFile(obj, current, i, iPad) {
+    console.log("[" + iPad + "] Checking " + (current.name ? current.name : current.url) + " file integrity.");
     if (current.file) {
         md5File(obj.config.folder + "/" + current.file, (err, md5) => {
             if (err) {
                 if (err.code === "ENOENT") {
-                    console.log("[" + i + '] WARNING: "' + current.file + '" could not be found. Was it deleted?');
+                    console.log("[" + iPad + '] WARNING: "' + current.file + '" could not be found. Was it deleted?');
                     return; // Nothing to check. Stop.
                 } else throw err;
             }
             if (!current.md5) {
-                console.log("[" + i + '] Missing MD5 for previously downloaded file: "' + current.file + '". Please delete the file and let it redownload.');
+                console.log("[" + iPad + '] Missing MD5 for previously downloaded file: "' + current.file + '". Please delete the file and let it redownload.');
                 throw new Error("Missing MD5");
             }
             if (current.md5 !== md5) {
-                console.log("[" + i + '] MD5 mismatch on previously downloaded file: "' + current.file + '". Please delete the file and let it redownload.');
+                console.log("[" + iPad + '] MD5 mismatch on previously downloaded file: "' + current.file + '". Please delete the file and let it redownload.');
                 throw new Error("MD5 mismatch");
             }
-            console.log("[" + i + "] Successfully checked " + (current.name ? current.name : current.url) + " file integrity.");
+            console.log("[" + iPad + "] Successfully checked " + (current.name ? current.name : current.url) + " file integrity.");
         });
     } else {
-        console.log("[" + i + "] Skipping file integrity check. " + (current.name ? current.name : current.url) + " has not been downloaded yet.");
+        console.log("[" + iPad + "] Skipping file integrity check. " + (current.name ? current.name : current.url) + " has not been downloaded yet.");
     }
 }
 
-function parseDownload(obj, current, i, temp, $) {
+function parseDownload(obj, current, i, iPad, temp, $) {
     temp = (typeof temp === 'undefined' ? {} : temp);
     fs.readFile("drivers/" + url.parse(current.url).host + ".js", "utf8", function(err, script) {
-        console.log("[" + i + "] Checking " + (current.name ? current.name : current.url) + " for updates.");
+        console.log("[" + iPad + "] Checking " + (current.name ? current.name : current.url) + " for updates.");
         if (err) throw err;
         eval(script); // There must be a better way to do this.
     });
