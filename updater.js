87149ab19e3745a7639c08a30fa031f3589101e6
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2018-03-07 16:25:16 +0100

    Add a delayed log that sorts the file thread messages

--- updater.js
+++ updater.js
@@ -6,10 +6,26 @@ var cheerio = require("cheerio");
 var request = require("request");
 var md5File = require("md5-file");
 var obj;
+var delayedLog = [];
 
 fs.readFile("downloadlist.json", "utf8", function(err, data) {
     if (err) throw err;
     obj = JSON.parse(data);
+    console._log = console.log;
+    if (obj.config["delayed-log"]) {
+        console.log = function() {
+            if (arguments.length && typeof arguments[0] === "string") {
+                if (arguments[0].startsWith("[")) {
+                    delayedLog.push(Array.prototype.slice.call(arguments).join(" "));
+                    return;
+                }
+                console._log.apply(this, arguments);
+                return;
+            }
+            console._log.apply(this, arguments);
+            return;
+        }
+    }
     fs.ensureDirSync(obj.config.folder);
     var iPad = 0;
     var current;
@@ -64,6 +80,16 @@ function parseDownload(obj, current, i, iPad, temp, $) {
 }
 
 process.on('exit', function() {
+    var delayedLogCopy = delayedLog.slice();
+    delayedLog.sort(function(a, b) {
+        var aInt = parseInt(a.substring(1, obj.downloads.length.toString().length + 1));
+        var bInt = parseInt(b.substring(1, obj.downloads.length.toString().length + 1));
+        if (aInt === bInt) return delayedLogCopy.indexOf(a) - delayedLogCopy.indexOf(b);
+        return aInt - bInt;
+    });
+    while (delayedLog.length) {
+        console._log(delayedLog.shift());
+    }
     obj.downloads.sort((a, b) => a.name.localeCompare(b.name, 'en', {
         sensitivity: 'base'
     }));
