a430ffe1b2412d3120c2d48abacf273d124eb694
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2018-03-07 11:45:14 +0100

    Enable and disable mods even if there are no updates

--- drivers/shadersmod.net.js
+++ drivers/shadersmod.net.js
@@ -7,15 +7,33 @@ request(current.url, function(err, response, html) {
     $ = cheerio.load(html);
     temp.href = $("h2.header-version:contains(DOWNLOADS)").nextAll("p").find("a").attr("href");
     temp.file = temp.href.substring(temp.href.lastIndexOf("/") + 1);
-    if (current.file === temp.file && fs.existsSync(obj.config.folder + "/" + temp.file)) {
+    if (current.file === temp.file && fs.existsSync(obj.config.folder + "/" + temp.file)) { // Nothing to update.
         console.log("[" + iPad + "] " + (current.name ? current.name : current.url) + " is already up to date.");
-        return;
+        if (!current.disabled && current.file.endsWith(".disabled")) {
+            temp.file = current.file.substring(0, current.file.length - 9);
+            console.log("[" + iPad + "] Enabling " + (current.name ? current.name : current.url) + ".");
+            fs.rename(obj.config.folder + "/" + current.file, obj.config.folder + "/" + temp.file, function(err) {
+                if (err) throw err;
+                current.file = temp.file;
+            });
+            return; // File has been enabled. Stop.
+        }
+        if (current.disabled && !current.file.endsWith(".disabled")) {
+            temp.file = current.file + ".disabled";
+            console.log("[" + iPad + "] Disabling " + (current.name ? current.name : current.url) + ".");
+            fs.rename(obj.config.folder + "/" + current.file, obj.config.folder + "/" + temp.file, function(err) {
+                if (err) throw err;
+                current.file = temp.file;
+            });
+            return; // File has been disabled. Stop.
+        }
+        return; // Nothing more to do. Stop.
     }
     if (current.file && fs.existsSync(obj.config.folder + "/" + current.file)) {
         console.log("[" + iPad + '] Deleting outdated file: "' + current.file + '".');
         fs.unlinkSync(obj.config.folder + "/" + current.file);
     }
-    if(current.disabled) temp.file = temp.file + ".disabled";
+    if (current.disabled) temp.file = temp.file + ".disabled";
     console.log("[" + iPad + "] Downloading: " + temp.href + ' as "' + temp.file + '"');
     request(temp.href).pipe(fs.createWriteStream(obj.config.folder + "/" + temp.file)).on("finish", function() {
         current.md5 = md5File.sync(this.path);
