b85c4342ab92ce8942e37bce80a33d4bebdefd35
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2018-03-17 15:25:30 +0100

    Move checkFile into a seperate file

---
+++ func/checkFile.js
@@ -0,0 +1,41 @@
+(function() {
+
+// Libraries
+var fs = require("fs-extra");
+var md5File = require("md5-file");
+
+module.exports = function(obj, current, i, iPad) {
+    if (current["folder-override"]) fs.ensureDirSync(current["folder-override"]);
+    console.log("[" + iPad + "] Checking " + (current.name ? current.name : (current.url ? current.url : (current.file ? current.file : i + ": '" + JSON.stringify(current) + "'"))) + " file integrity.");
+    if (current.file) {
+        md5File((current["folder-override"] ? current["folder-override"] : obj.config.folder) + "/" + current.file, (err, md5) => {
+            if (err) {
+                if (err.code === "ENOENT") {
+                    if (!current.url) {
+                        console.log("[" + iPad + '] ERROR: "' + (current.name ? current.name : current.file) + '" is missing its file and has no URL to update from.');
+                        throw new Error("Missing file");
+                    }
+                    console.log("[" + iPad + '] WARNING: "' + current.file + '" could not be found. Was it deleted?');
+                    return; // Nothing to check. Stop.
+                } else throw err;
+            }
+            if (!current.url) console.log("[" + iPad + '] WARNING: "' + (current.name ? current.name : current.file) + ' has no URL. It will not be updated.');
+            if (!current.md5) {
+                console.log("[" + iPad + '] Missing MD5 for previously downloaded file: "' + current.file + '". Please delete the file and let it redownload.');
+                throw new Error("Missing MD5");
+            }
+            if (current.md5 !== md5) {
+                console.log("[" + iPad + '] MD5 mismatch on previously downloaded file: "' + current.file + '". Please delete the file and let it redownload.');
+                throw new Error("MD5 mismatch");
+            }
+            console.log("[" + iPad + "] Successfully checked " + (current.name ? current.name : current.url) + " file integrity.");
+        });
+    } else if (current.url) {
+        console.log("[" + iPad + "] Skipping file integrity check. " + (current.name ? current.name : current.url) + " has not been downloaded yet.");
+    } else {
+        console.log("[" + iPad + '] ERROR: "' + (current.name ? current.name : (current.url ? current.url : (current.file ? current.file : i + ": '" + JSON.stringify(current) + "'"))) + '" has no configured file or URL.');
+        throw new Error("Missing file");
+    }
+}
+
+})();
--- updater.js
+++ updater.js
@@ -1,10 +1,16 @@
 (function() {
 
+// Libraries
 var fs = require("fs-extra");
 var url = require("url");
 var cheerio = require("cheerio");
 var request = require("request");
 var md5File = require("md5-file");
+
+// Functions
+var checkFile = require("./func/checkFile.js");
+
+// Variables
 var obj;
 var delayedLog = [];
 
@@ -36,40 +42,6 @@ fs.readFile("downloadlist.json", "utf8", function(err, data) {
         if (current.url) parseDownload(obj, current, i, iPad);
     }
 });
-
-function checkFile(obj, current, i, iPad) {
-    if (current["folder-override"]) fs.ensureDirSync(current["folder-override"]);
-    console.log("[" + iPad + "] Checking " + (current.name ? current.name : (current.url ? current.url : (current.file ? current.file : i + ": '" + JSON.stringify(current) + "'"))) + " file integrity.");
-    if (current.file) {
-        md5File((current["folder-override"] ? current["folder-override"] : obj.config.folder) + "/" + current.file, (err, md5) => {
-            if (err) {
-                if (err.code === "ENOENT") {
-                    if (!current.url) {
-                        console.log("[" + iPad + '] ERROR: "' + (current.name ? current.name : current.file) + '" is missing its file and has no URL to update from.');
-                        throw new Error("Missing file");
-                    }
-                    console.log("[" + iPad + '] WARNING: "' + current.file + '" could not be found. Was it deleted?');
-                    return; // Nothing to check. Stop.
-                } else throw err;
-            }
-            if (!current.url) console.log("[" + iPad + '] WARNING: "' + (current.name ? current.name : current.file) + ' has no URL. It will not be updated.');
-            if (!current.md5) {
-                console.log("[" + iPad + '] Missing MD5 for previously downloaded file: "' + current.file + '". Please delete the file and let it redownload.');
-                throw new Error("Missing MD5");
-            }
-            if (current.md5 !== md5) {
-                console.log("[" + iPad + '] MD5 mismatch on previously downloaded file: "' + current.file + '". Please delete the file and let it redownload.');
-                throw new Error("MD5 mismatch");
-            }
-            console.log("[" + iPad + "] Successfully checked " + (current.name ? current.name : current.url) + " file integrity.");
-        });
-    } else if (current.url) {
-        console.log("[" + iPad + "] Skipping file integrity check. " + (current.name ? current.name : current.url) + " has not been downloaded yet.");
-    } else {
-        console.log("[" + iPad + '] ERROR: "' + (current.name ? current.name : (current.url ? current.url : (current.file ? current.file : i + ": '" + JSON.stringify(current) + "'"))) + '" has no configured file or URL.');
-        throw new Error("Missing file");
-    }
-}
 
 function parseDownload(obj, current, i, iPad, temp, $) {
     temp = (typeof temp === 'undefined' ? {} : temp);
