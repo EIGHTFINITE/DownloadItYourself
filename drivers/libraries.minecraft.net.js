942488709638808332269c24be02461d035f89f6
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2018-03-18 11:47:52 +0100

    Support LZMA download

--- drivers/libraries.minecraft.net.js
+++ drivers/libraries.minecraft.net.js
@@ -8,5 +8,6 @@ if (!current.name) {
     if(temp.file === "asm-all-5.0.3.jar") current.name = "ASM";
     else if(temp.file === "guava-17.0.jar") current.name = "Guava";
     else if(temp.file === "launchwrapper-1.12.jar") current.name = "LaunchWrapper";
+    else if(temp.file === "lzma-0.0.1.jar") current.name = "LZMA";
 };
 if (!("disabled" in current)) current.disabled = false;
