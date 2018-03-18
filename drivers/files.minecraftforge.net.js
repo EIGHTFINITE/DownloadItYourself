c13897a3fc3d00ac98fbef60cd4e1d1173d0426f
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2018-03-18 17:15:01 +0100

    Support Akka Actor download

--- drivers/files.minecraftforge.net.js
+++ drivers/files.minecraftforge.net.js
@@ -1,16 +1,27 @@
 /**
  * Driver for downloading from files.minecraftforge.net
  */
-request(current.url, function(err, response, html) {
-    console.log("[" + iPad + "] Navigating to: " + current.url);
-    if (err) throw err;
-    $ = cheerio.load(html);
-    if (current.preview === true) temp.href = $(".downloads>.download>.title>.promo-latest").parent().parent().find(".links .link .classifier-universal").parent().attr("href");
-    else temp.href = $(".downloads>.download>.title>.promo-recommended").parent().parent().find(".links .link .classifier-universal").parent().attr("href");
-    temp.href = temp.href.substring(temp.href.indexOf('&url=') + 5);
+if (current.url.startsWith("http://")) current.url = current.url.replace("http://", "https://");
+if (current.url.endsWith(".html")) {
+    request(current.url, function(err, response, html) {
+        console.log("[" + iPad + "] Navigating to: " + current.url);
+        if (err) throw err;
+        $ = cheerio.load(html);
+        if (current.preview === true) temp.href = $(".downloads>.download>.title>.promo-latest").parent().parent().find(".links .link .classifier-universal").parent().attr("href");
+        else temp.href = $(".downloads>.download>.title>.promo-recommended").parent().parent().find(".links .link .classifier-universal").parent().attr("href");
+        temp.href = temp.href.substring(temp.href.indexOf('&url=') + 5);
+        temp.file = temp.href.substring(temp.href.lastIndexOf("/") + 1);
+        downloadFile(obj, current, i, iPad, temp);
+        if (!current.name) current.name = "Forge/FML";
+        if (!("disabled" in current)) current.disabled = false;
+        if (!("preview" in current)) current.preview = false;
+    });
+} else {
+    temp.href = current.url;
     temp.file = temp.href.substring(temp.href.lastIndexOf("/") + 1);
     downloadFile(obj, current, i, iPad, temp);
-    if (!current.name) current.name = "Forge/FML";
+    if (!current.name) {
+        if (temp.file.includes("akka-actor")) current.name = "Akka Actor";
+    }
     if (!("disabled" in current)) current.disabled = false;
-    if (!("preview" in current)) current.preview = false;
-});
+}
