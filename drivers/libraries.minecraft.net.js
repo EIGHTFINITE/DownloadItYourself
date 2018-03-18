e5afe68ef2a80295ff290e9f1a049daa11fa1d9a
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2018-03-18 10:57:31 +0100

    Support Guava download

--- drivers/libraries.minecraft.net.js
+++ drivers/libraries.minecraft.net.js
@@ -6,6 +6,7 @@ temp.file = temp.href.substring(temp.href.lastIndexOf("/") + 1);
 downloadFile(obj, current, i, iPad, temp);
 if (!current.name) {
     if(temp.file === "asm-all-5.0.3.jar") current.name = "ASM";
+    else if(temp.file === "guava-17.0.jar") current.name = "Guava";
     else if(temp.file === "launchwrapper-1.12.jar") current.name = "LaunchWrapper";
 };
 if (!("disabled" in current)) current.disabled = false;
