baa29063ed35db12ef4ff41448c3561134837c9a
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2018-03-22 00:28:53 +0100

    Create a new function for logging messages from the file threads

--- drivers/optifine.net.js
+++ drivers/optifine.net.js
@@ -2,18 +2,18 @@
  * Driver for downloading from optifine.net
  */
 request(current.url, function(err, response, html) {
-    console.log("[" + iPad + "] Navigating to: " + current.url);
+    console.message("Navigating to: " + current.url, i);
     if (err) throw err;
     $ = cheerio.load(html);
     if (current.preview === true) temp.href = $("span#preview td.downloadLineFile:contains(1.7.10)").nextAll("td.downloadLineMirror").children("a").attr("href");
     else temp.href = $("h2:contains(" + global.config.version + ")").nextAll("table.downloadTable").first().find("td.downloadLineMirror a").attr("href");
     request(temp.href, function(err, response, html) {
-        console.log("[" + iPad + "] Navigating to: " + temp.href);
+        console.message("Navigating to: " + temp.href, i);
         if (err) throw err;
         $ = cheerio.load(html);
         temp.file = $("span#Download a").text().trim().replace(/^(Download )/, "").trim();
         temp.href = response.request.uri.protocol + "//" + response.request.uri.host + "/" + $("span#Download a").attr("href");
-        downloadFile(current, i, iPad, temp);
+        downloadFile(i, current, temp);
     });
     if (!current.name) current.name = $("h2:contains(" + global.config.version + ")").next("h3").text().trim();
     if (!("disabled" in current)) current.disabled = false;
--- func/downloadFile.js
+++ func/downloadFile.js
@@ -5,17 +5,17 @@ var fs = require("fs-extra");
 var request = require("request");
 var md5File = require("md5-file");
 
-module.exports = function(current, i, iPad, temp) {
+module.exports = function(i, current, temp) {
     // Override filename
     // Yes, applies to additional folders too. This is intended.
     temp.originalFilename = temp.file;
     if (current["file-override"]) temp.file = current["file-override"];
     // Are we about to download the same file we already have?
     if ((current.md5 ? true : current.md5 === temp.md5) && (current.file === temp.file || current.file === temp.file + ".disabled") && fs.existsSync((current["folder-override"] ? "../" + current["folder-override"] : "../" + global.config.folder) + "/" + current.file)) { // Nothing to update.
-        console.log("[" + iPad + "] " + (current.name ? current.name : current.url) + " is already up to date.");
+        console.message((current.name ? current.name : current.url) + " is already up to date.", i);
         if (!current.disabled && current.file.endsWith(".disabled")) {
             temp.file = current.file.substring(0, current.file.length - 9);
-            console.log("[" + iPad + "] Enabling " + (current.name ? current.name : current.url) + ".");
+            console.message("Enabling " + (current.name ? current.name : current.url) + ".", i);
             fs.rename((current["folder-override"] ? "../" + current["folder-override"] : "../" + global.config.folder) + "/" + current.file, (current["folder-override"] ? "../" + current["folder-override"] : "../" + global.config.folder) + "/" + temp.file, function(err) {
                 if (err) throw err;
                 current.file = temp.file;
@@ -24,7 +24,7 @@ module.exports = function(current, i, iPad, temp) {
         }
         if (current.disabled && !current.file.endsWith(".disabled")) {
             temp.file = current.file + ".disabled";
-            console.log("[" + iPad + "] Disabling " + (current.name ? current.name : current.url) + ".");
+            console.message("Disabling " + (current.name ? current.name : current.url) + ".", i);
             fs.rename((current["folder-override"] ? "../" + current["folder-override"] : "../" + global.config.folder) + "/" + current.file, (current["folder-override"] ? "../" + current["folder-override"] : "../" + global.config.folder) + "/" + temp.file, function(err) {
                 if (err) throw err;
                 current.file = temp.file;
@@ -35,33 +35,33 @@ module.exports = function(current, i, iPad, temp) {
     }
     // Do we have an outdated file?
     if (current.file && fs.existsSync((current["folder-override"] ? "../" + current["folder-override"] : "../" + global.config.folder) + "/" + current.file)) {
-        console.log("[" + iPad + '] Deleting outdated file: "' + current.file + '".');
+        console.message('Deleting outdated file: "' + current.file + '".', i);
         fs.unlinkSync((current["folder-override"] ? "../" + current["folder-override"] : "../" + global.config.folder) + "/" + current.file);
         // Any outdated files in the additional folders?
         if (current.file && fs.existsSync("../" + current["additional-folder"] + "/" + current.file)) {
-            console.log("[" + iPad + '] Deleting outdated file: "' + current.file + '".');
+            console.message('Deleting outdated file: "' + current.file + '".', i);
             fs.unlinkSync("../" + current["additional-folder"] + "/" + current.file);
         }
     }
     // Should the file be disabled on creation?
     if (current.disabled) temp.file = temp.file + ".disabled";
     // Let's finally download this thing.
-    console.log("[" + iPad + "] Downloading: " + temp.href + ' as "' + temp.file + '"');
+    console.message("Downloading: " + temp.href + ' as "' + temp.file + '"', i);
     fs.ensureDirSync(current["folder-override"] ? "../" + current["folder-override"] : "../" + global.config.folder);
     request(temp.href).pipe(fs.createWriteStream((current["folder-override"] ? "../" + current["folder-override"] : "../" + global.config.folder) + "/" + temp.file)).on("finish", function() {
         current.md5 = md5File.sync(this.path);
         // Check if downloaded file matches expected MD5.
         if ("md5" in temp ? current.md5 !== temp.md5 : false) {
-            console.log("[" + iPad + "] ERROR: MD5 mismatch for " + (current.name ? current.name : current.url) + ". Download failed.");
+            console.message("ERROR: MD5 mismatch for " + (current.name ? current.name : current.url) + ". Download failed.", i);
             throw new Error("MD5 mismatch");
         }
         // Create copies of file in the additional folders.
         if (current["additional-folder"]) {
             fs.copySync((current["folder-override"] ? "../" + current["folder-override"] : "../" + global.config.folder) + "/" + temp.file, "../" + current["additional-folder"] + "/" + temp.file);
-            console.log("[" + iPad + "] Copied " + (current.name ? current.name : current.url) + " to '../" + current["additional-folder"] + "'");
+            console.message("Copied " + (current.name ? current.name : current.url) + " to '../" + current["additional-folder"] + "'", i);
         }
         // Update successful.
-        console.log("[" + iPad + "] " + (current.name ? current.name : current.url) + " has successfully updated." + ("md5" in temp ? " (MD5 matches)" : ""));
+        console.message((current.name ? current.name : current.url) + " has successfully updated." + ("md5" in temp ? " (MD5 matches)" : ""), i);
         // Update file location.
         current.file = temp.originalFilename + (current.disabled ? ".disabled" : "");
     });
