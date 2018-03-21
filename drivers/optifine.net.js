86c0a2b0bf6745fc71431a937ae16eae4512ddb2
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2018-03-21 23:21:19 +0100

    Shorter variable to access downloads and config

--- drivers/optifine.net.js
+++ drivers/optifine.net.js
@@ -6,7 +6,7 @@ request(current.url, function(err, response, html) {
     if (err) throw err;
     $ = cheerio.load(html);
     if (current.preview === true) temp.href = $("span#preview td.downloadLineFile:contains(1.7.10)").nextAll("td.downloadLineMirror").children("a").attr("href");
-    else temp.href = $("h2:contains(" + global.list.config.version + ")").nextAll("table.downloadTable").first().find("td.downloadLineMirror a").attr("href");
+    else temp.href = $("h2:contains(" + global.config.version + ")").nextAll("table.downloadTable").first().find("td.downloadLineMirror a").attr("href");
     request(temp.href, function(err, response, html) {
         console.log("[" + iPad + "] Navigating to: " + temp.href);
         if (err) throw err;
@@ -15,7 +15,7 @@ request(current.url, function(err, response, html) {
         temp.href = response.request.uri.protocol + "//" + response.request.uri.host + "/" + $("span#Download a").attr("href");
         downloadFile(current, i, iPad, temp);
     });
-    if (!current.name) current.name = $("h2:contains(" + global.list.config.version + ")").next("h3").text().trim();
+    if (!current.name) current.name = $("h2:contains(" + global.config.version + ")").next("h3").text().trim();
     if (!("disabled" in current)) current.disabled = false;
     if (!("preview" in current)) current.preview = false;
 });
