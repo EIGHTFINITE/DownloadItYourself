7d8792424743c640967f726b679009e9a4ea3bb6
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2018-03-21 23:07:21 +0100

    We need access to the downloadlist everywhere so lets just use a global

--- drivers/files.minecraftforge.net.js
+++ drivers/files.minecraftforge.net.js
@@ -11,7 +11,7 @@ if (current.url.endsWith(".html")) {
         else temp.href = $(".downloads>.download>.title>.promo-recommended").parent().parent().find(".links .link .classifier-universal").parent().attr("href");
         temp.href = temp.href.substring(temp.href.indexOf('&url=') + 5);
         temp.file = temp.href.substring(temp.href.lastIndexOf("/") + 1);
-        downloadFile(obj, current, i, iPad, temp);
+        downloadFile(current, i, iPad, temp);
         if (!current.name) current.name = "Forge/FML";
         if (!("disabled" in current)) current.disabled = false;
         if (!("preview" in current)) current.preview = false;
@@ -19,7 +19,7 @@ if (current.url.endsWith(".html")) {
 } else {
     temp.href = current.url;
     temp.file = temp.href.substring(temp.href.lastIndexOf("/") + 1);
-    downloadFile(obj, current, i, iPad, temp);
+    downloadFile(current, i, iPad, temp);
     if (!current.name) {
         if (temp.file.includes("akka-actor")) current.name = "Akka Actor";
         if (temp.file.includes("config")) current.name = "Typesafe Config";
--- drivers/s3.amazonaws.com.js
+++ drivers/s3.amazonaws.com.js
@@ -3,7 +3,7 @@
  */
 temp.href = current.url;
 temp.file = temp.href.substring(temp.href.lastIndexOf("/") + 1);
-downloadFile(obj, current, i, iPad, temp);
+downloadFile(current, i, iPad, temp);
 if (!current.name) {
 	if(temp.file === "minecraft_server.1.7.10.jar") current.name = "Minecraft Server";
 };
