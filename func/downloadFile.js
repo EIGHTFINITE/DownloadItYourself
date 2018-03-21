9c15b9b3c3faa3aed6a646770c2c542b9d0f90e9
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2018-03-21 14:29:44 +0100

    Update the rest of the code to take the new working directory into account

--- func/downloadFile.js
+++ func/downloadFile.js
@@ -11,12 +11,12 @@ module.exports = function(obj, current, i, iPad, temp) {
     temp.originalFilename = temp.file;
     if (current["file-override"]) temp.file = current["file-override"];
     // Are we about to download the same file we already have?
-    if ((current.md5 ? true : current.md5 === temp.md5) && (current.file === temp.file || current.file === temp.file + ".disabled") && fs.existsSync((current["folder-override"] ? current["folder-override"] : obj.config.folder) + "/" + current.file)) { // Nothing to update.
+    if ((current.md5 ? true : current.md5 === temp.md5) && (current.file === temp.file || current.file === temp.file + ".disabled") && fs.existsSync((current["folder-override"] ? "../" + current["folder-override"] : "../" + obj.config.folder) + "/" + current.file)) { // Nothing to update.
         console.log("[" + iPad + "] " + (current.name ? current.name : current.url) + " is already up to date.");
         if (!current.disabled && current.file.endsWith(".disabled")) {
             temp.file = current.file.substring(0, current.file.length - 9);
             console.log("[" + iPad + "] Enabling " + (current.name ? current.name : current.url) + ".");
-            fs.rename((current["folder-override"] ? current["folder-override"] : obj.config.folder) + "/" + current.file, (current["folder-override"] ? current["folder-override"] : obj.config.folder) + "/" + temp.file, function(err) {
+            fs.rename((current["folder-override"] ? "../" + current["folder-override"] : "../" + obj.config.folder) + "/" + current.file, (current["folder-override"] ? "../" + current["folder-override"] : "../" + obj.config.folder) + "/" + temp.file, function(err) {
                 if (err) throw err;
                 current.file = temp.file;
             });
@@ -25,7 +25,7 @@ module.exports = function(obj, current, i, iPad, temp) {
         if (current.disabled && !current.file.endsWith(".disabled")) {
             temp.file = current.file + ".disabled";
             console.log("[" + iPad + "] Disabling " + (current.name ? current.name : current.url) + ".");
-            fs.rename((current["folder-override"] ? current["folder-override"] : obj.config.folder) + "/" + current.file, (current["folder-override"] ? current["folder-override"] : obj.config.folder) + "/" + temp.file, function(err) {
+            fs.rename((current["folder-override"] ? "../" + current["folder-override"] : "../" + obj.config.folder) + "/" + current.file, (current["folder-override"] ? "../" + current["folder-override"] : "../" + obj.config.folder) + "/" + temp.file, function(err) {
                 if (err) throw err;
                 current.file = temp.file;
             });
@@ -34,20 +34,21 @@ module.exports = function(obj, current, i, iPad, temp) {
         return; // Nothing more to do. Stop.
     }
     // Do we have an outdated file?
-    if (current.file && fs.existsSync((current["folder-override"] ? current["folder-override"] : obj.config.folder) + "/" + current.file)) {
+    if (current.file && fs.existsSync((current["folder-override"] ? "../" + current["folder-override"] : "../" + obj.config.folder) + "/" + current.file)) {
         console.log("[" + iPad + '] Deleting outdated file: "' + current.file + '".');
-        fs.unlinkSync((current["folder-override"] ? current["folder-override"] : obj.config.folder) + "/" + current.file);
+        fs.unlinkSync((current["folder-override"] ? "../" + current["folder-override"] : "../" + obj.config.folder) + "/" + current.file);
         // Any outdated files in the additional folders?
-        if (current.file && fs.existsSync(current["additional-folder"] + "/" + current.file)) {
+        if (current.file && fs.existsSync("../" + current["additional-folder"] + "/" + current.file)) {
             console.log("[" + iPad + '] Deleting outdated file: "' + current.file + '".');
-            fs.unlinkSync(current["additional-folder"] + "/" + current.file);
+            fs.unlinkSync("../" + current["additional-folder"] + "/" + current.file);
         }
     }
     // Should the file be disabled on creation?
     if (current.disabled) temp.file = temp.file + ".disabled";
     // Let's finally download this thing.
     console.log("[" + iPad + "] Downloading: " + temp.href + ' as "' + temp.file + '"');
-    request(temp.href).pipe(fs.createWriteStream((current["folder-override"] ? current["folder-override"] : obj.config.folder) + "/" + temp.file)).on("finish", function() {
+    fs.ensureDirSync(current["folder-override"] ? "../" + current["folder-override"] : "../" + obj.config.folder);
+    request(temp.href).pipe(fs.createWriteStream((current["folder-override"] ? "../" + current["folder-override"] : "../" + obj.config.folder) + "/" + temp.file)).on("finish", function() {
         current.md5 = md5File.sync(this.path);
         // Check if downloaded file matches expected MD5.
         if ("md5" in temp ? current.md5 !== temp.md5 : false) {
@@ -56,8 +57,8 @@ module.exports = function(obj, current, i, iPad, temp) {
         }
         // Create copies of file in the additional folders.
         if (current["additional-folder"]) {
-            fs.copySync((current["folder-override"] ? current["folder-override"] : obj.config.folder) + "/" + temp.file, current["additional-folder"] + "/" + temp.file);
-            console.log("[" + iPad + "] Copied " + (current.name ? current.name : current.url) + " to '" + current["additional-folder"] + "'");
+            fs.copySync((current["folder-override"] ? "../" + current["folder-override"] : "../" + obj.config.folder) + "/" + temp.file, "../" + current["additional-folder"] + "/" + temp.file);
+            console.log("[" + iPad + "] Copied " + (current.name ? current.name : current.url) + " to '../" + current["additional-folder"] + "'");
         }
         // Update successful.
         console.log("[" + iPad + "] " + (current.name ? current.name : current.url) + " has successfully updated." + ("md5" in temp ? " (MD5 matches)" : ""));
--- func/parseDownload.js
+++ func/parseDownload.js
@@ -11,7 +11,7 @@ var downloadFile =  require("../func/downloadFile.js");
 
 module.exports = function(obj, current, i, iPad, temp, $) {
     temp = (typeof temp === 'undefined' ? {} : temp);
-    fs.readFile("drivers/" + url.parse(current.url).host + ".js", "utf8", function(err, script) {
+    fs.readFile("../drivers/" + url.parse(current.url).host + ".js", "utf8", function(err, script) {
         console.log("[" + iPad + "] Checking " + (current.name ? current.name : current.url) + " for updates.");
         if (err) throw err;
         eval(script); // There must be a better way to do this.
