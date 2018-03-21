378dc769b9881267397cae6f140930e28f53a0c9
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2018-03-22 00:28:53 +0100

    Create a new function for logging messages from the file threads

--- drivers/shadersmod.net.js
+++ drivers/shadersmod.net.js
@@ -2,12 +2,12 @@
  * Driver for downloading from shadersmod.net
  */
 request(current.url, function(err, response, html) {
-    console.log("[" + iPad + "] Navigating to: " + current.url);
+    console.message("Navigating to: " + current.url, i);
     if (err) throw err;
     $ = cheerio.load(html);
     temp.href = $("h2.header-version:contains(DOWNLOADS)").nextAll("p").find("a").attr("href");
     temp.file = temp.href.substring(temp.href.lastIndexOf("/") + 1);
-    downloadFile(current, i, iPad, temp);
+    downloadFile(i, current, temp);
     if (!current.name) current.name = "Shaders Mod";
     if (!("disabled" in current)) current.disabled = false;
 });
