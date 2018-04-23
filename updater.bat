ebb7fb53436f67950378efe75ff7953b83149e1e
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2018-04-23 16:00:22 +0200

    Show npm output

--- updater.bat
+++ updater.bat
@@ -2,5 +2,5 @@
 setlocal
 cd /d %~dp0
 reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | "%windir%\System32\find.exe" /i "x86" > nul && set osArchitecture=86 || set osArchitecture=64
-cmd /c bin\windows\x%osArchitecture%\node\npm install 1>nul 2>nul
+cmd /c bin\windows\x%osArchitecture%\node\npm install
 bin\windows\x%osArchitecture%\node\node --use_strict updater.js
