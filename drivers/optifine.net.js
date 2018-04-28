2cb0422996b214ef49732f08d065e5ad386e98cf
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2018-04-28 15:48:42 +0200

    Mark various console messages as verbose

--- drivers/optifine.net.js
+++ drivers/optifine.net.js
@@ -2,14 +2,14 @@
  * Driver for downloading from optifine.net
  */
 request(current.url, function(err, response, html) {
-    console.message("Navigating to: '" + current.url + "'.", i);
+    if(global.config.verbose) console.message("Navigating to: '" + current.url + "'.", i);
     if (err) throw err;
     $ = cheerio.load(html);
     var previewBuilds = $("span#preview td.downloadLineFile:contains(1.7.10)").nextAll("td.downloadLineMirror").children("a");
     if (current.preview === true && previewBuilds.length > 0) temp.href = previewBuilds.attr("href");
     else temp.href = $("h2:contains(" + global.config.version + ")").nextAll("table.downloadTable").first().find("td.downloadLineMirror a").attr("href");
     request(temp.href, function(err, response, html) {
-        console.message("Navigating to: '" + temp.href + "'.", i);
+        if(global.config.verbose) console.message("Navigating to: '" + temp.href + "'.", i);
         if (err) throw err;
         $ = cheerio.load(html);
         temp.file = $("span#Download a").text().trim().replace(/^(Download )/, "").trim();
