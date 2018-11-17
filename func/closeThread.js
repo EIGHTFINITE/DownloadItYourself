36000e203b1a1db2244f4c4c907f05cbed23d3fb
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2018-11-17 21:36:38 +0100

    Simplify delayed log

--- func/closeThread.js
+++ func/closeThread.js
@@ -1,6 +1,7 @@
 (function() {
 
 module.exports = function(i) {
+	if (typeof global.threads[i] === "undefined") throw new Error("Cannot close nonexistent thread.");
 	if (global.threads[i] === false) throw new Error("Thread has already been closed.");
     global.threads[i] = false;
 }
