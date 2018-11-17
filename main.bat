84fcf6cf32ce9669839df91728342b4242881ed5
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2018-11-17 15:47:12 +0100

    Rename main executable to simply 'main'

--- main.bat
+++ main.bat
@@ -3,4 +3,4 @@ setlocal
 cd /d %~dp0
 reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | "%windir%\System32\find.exe" /i "x86" > nul && set osArchitecture=86 || set osArchitecture=64
 cmd /c bin\windows\x%osArchitecture%\node\npm install
-bin\windows\x%osArchitecture%\node\node --use_strict updater.js
+bin\windows\x%osArchitecture%\node\node --use_strict main.js
