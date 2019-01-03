08bb4364f2bfe5162b8ce2157083ccd614df2ff4
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2019-01-03 22:14:14 +0100

    Handle downloading in a single function

--- drivers/minecraft.curseforge.com.js
+++ drivers/minecraft.curseforge.com.js
@@ -1,21 +1,20 @@
 /**
  * Driver for downloading from minecraft.curseforge.com
  */
-temp.href = current.url + "/files?" + (global.config["curseforge-version"] ? "filter-game-version=" + global.config["curseforge-version"] : "");
-request(temp.href, function(err, response, html) {
-    console.message(i, "Navigating to: '" + temp.href + "'.", MESSAGE_VERBOSE);
+temp.url = temp.url + "/files?" + (global.config["curseforge-version"] ? "filter-game-version=" + global.config["curseforge-version"] : "");
+request(temp.url, function(err, response, html) {
+    console.message(i, "Navigating to: '" + temp.url + "'.", MESSAGE_VERBOSE);
     if (err) throw err;
-    $ = cheerio.load(html);
-    temp.href = response.request.uri.protocol + "//" + response.request.uri.host + $("a.overflow-tip.twitch-link").attr("href");
-    request(temp.href, function(err, response, html) {
-        console.message(i, "Navigating to: '" + temp.href + "'.", MESSAGE_VERBOSE);
+    var $ = cheerio.load(html);
+    temp.url = response.request.uri.protocol + "//" + response.request.uri.host + $("a.overflow-tip.twitch-link").attr("href");
+    request(temp.url, function(err, response, html) {
+        console.message(i, "Navigating to: '" + temp.url + "'.", MESSAGE_VERBOSE);
         if (err) throw err;
         $ = cheerio.load(html);
         temp.md5 = $("span.md5").text().trim();
         temp.file = $("div.info-data.overflow-tip").text().trim();
-        temp.href = response.request.uri.protocol + "//" + response.request.uri.host + $("a.button.fa-icon-download:not(.alt)").attr("href");
+        temp.url = response.request.uri.protocol + "//" + response.request.uri.host + $("a.button.fa-icon-download:not(.alt)").attr("href");
         updateFile(i, current, temp);
     });
     if (!current.name) current.name = $("h1.project-title").text().trim();
-    if (!("disabled" in current)) current.disabled = false;
 });
--- drivers/optifine.net.js
+++ drivers/optifine.net.js
@@ -1,22 +1,21 @@
 /**
  * Driver for downloading from optifine.net
  */
-request(current.url, function(err, response, html) {
-    console.message(i, "Navigating to: '" + current.url + "'.", MESSAGE_VERBOSE);
+request(temp.url, function(err, response, html) {
+    console.message(i, "Navigating to: '" + temp.url + "'.", MESSAGE_VERBOSE);
     if (err) throw err;
-    $ = cheerio.load(html);
+    var $ = cheerio.load(html);
     var previewBuilds = $("span#preview td.downloadLineFile:contains(1.7.10)").nextAll("td.downloadLineMirror").children("a");
-    if (current.preview === true && previewBuilds.length > 0) temp.href = previewBuilds.attr("href");
-    else temp.href = $("h2:contains(" + global.config.version + ")").nextAll("table.downloadTable").first().find("td.downloadLineMirror a").attr("href");
-    request(temp.href, function(err, response, html) {
-        console.message(i, "Navigating to: '" + temp.href + "'.", MESSAGE_VERBOSE);
+    if (temp.preview === true && previewBuilds.length > 0) temp.url = previewBuilds.attr("href");
+    else temp.url = $("h2:contains(" + global.config.version + ")").nextAll("table.downloadTable").first().find("td.downloadLineMirror a").attr("href");
+    request(temp.url, function(err, response, html) {
+        console.message(i, "Navigating to: '" + temp.url + "'.", MESSAGE_VERBOSE);
         if (err) throw err;
         $ = cheerio.load(html);
         temp.file = $("span#Download a").text().trim().replace(/^(Download )/, "").trim();
-        temp.href = response.request.uri.protocol + "//" + response.request.uri.host + "/" + $("span#Download a").attr("href");
+        temp.url = response.request.uri.protocol + "//" + response.request.uri.host + "/" + $("span#Download a").attr("href");
         updateFile(i, current, temp);
     });
     if (!current.name) current.name = $("h2:contains(" + global.config.version + ")").next("h3").text().trim();
-    if (!("disabled" in current)) current.disabled = false;
     if (!("preview" in current)) current.preview = false;
 });
