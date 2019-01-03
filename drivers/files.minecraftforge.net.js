acafd59c5d2aaf627129c995077a39dddbefd2cd
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2019-01-03 22:14:14 +0100

    Handle downloading in a single function

--- drivers/files.minecraftforge.net.js
+++ drivers/files.minecraftforge.net.js
@@ -1,37 +1,18 @@
 /**
  * Driver for downloading from files.minecraftforge.net
  */
-if (current.url.startsWith("http://")) current.url = current.url.replace("http://", "https://");
-if (current.url.endsWith(".html")) {
-    request(current.url, function(err, response, html) {
-        console.message(i, "Navigating to: '" + current.url + "'.", MESSAGE_VERBOSE);
-        if (err) throw err;
-        $ = cheerio.load(html);
-        if (current.preview === true) temp.href = $(".downloads>.download>.title>.promo-latest").parent().parent().find(".links .link .classifier-universal").parent().attr("href");
-        else temp.href = $(".downloads>.download>.title>.promo-recommended").parent().parent().find(".links .link .classifier-universal").parent().attr("href");
-        temp.href = temp.href.substring(temp.href.indexOf('&url=') + 5);
-        temp.file = temp.href.substring(temp.href.lastIndexOf("/") + 1);
-        updateFile(i, current, temp);
-        if (!current.name) current.name = "Forge/FML";
-        if (!("disabled" in current)) current.disabled = false;
-        if (!("preview" in current)) current.preview = false;
-    });
-} else {
-    temp.href = current.url;
-    temp.file = temp.href.substring(temp.href.lastIndexOf("/") + 1);
-    updateFile(i, current, temp);
-    if (!current.name) {
-        if (temp.file.includes("akka-actor")) current.name = "Akka Actor";
-        if (temp.file.includes("config")) current.name = "Typesafe Config";
-        if (temp.file.includes("scala-actors-migration")) current.name = "Scala Actor Migration Kit";
-        if (temp.file.includes("scala-compiler")) current.name = "Scala Compiler API";
-        if (temp.file.includes("scala-continuations-library")) current.name = "Scala Continuations Library";
-        if (temp.file.includes("scala-continuations-plugin")) current.name = "Scala Continuations Plugin";
-        if (temp.file.includes("scala-library")) current.name = "Scala Library API";
-        if (temp.file.includes("scala-parser-combinators")) current.name = "Scala Standard Parser Combinator Library";
-        if (temp.file.includes("scala-reflect")) current.name = "Scala Reflection API";
-        if (temp.file.includes("scala-swing")) current.name = "Scala Swing API";
-        if (temp.file.includes("scala-xml")) current.name = "Scala XML API";
-    }
-    if (!("disabled" in current)) current.disabled = false;
-}
+if (temp.url.startsWith("http://"))
+	temp.url = temp.url.replace("http://", "https://");
+if (current.url.startsWith("http://"))
+	current.url = current.url.replace("http://", "https://");
+request(temp.url, function(err, response, html) {
+	console.message(i, "Navigating to: '" + temp.url + "'.", MESSAGE_VERBOSE);
+	if (err) throw err;
+	var $ = cheerio.load(html);
+	if (temp.preview === true) temp.url = $(".downloads>.download>.title>.promo-latest").parent().parent().find(".links .link .classifier-universal").parent().attr("href");
+	else temp.url = $(".downloads>.download>.title>.promo-recommended").parent().parent().find(".links .link .classifier-universal").parent().attr("href");
+	temp.url = temp.url.substring(temp.url.indexOf('&url=') + 5);
+	temp.file = temp.url.substring(temp.url.lastIndexOf("/") + 1);
+	updateFile(i, current, temp);
+	if (!("preview" in current)) current.preview = false;
+});
--- drivers/shadersmod.net.js
+++ drivers/shadersmod.net.js
@@ -1,13 +1,13 @@
 /**
  * Driver for downloading from shadersmod.net
  */
-request(current.url, function(err, response, html) {
-    console.message(i, "Navigating to: '" + current.url + "'.", MESSAGE_VERBOSE);
+request(temp.url, function(err, response, html) {
+    console.message(i, "Navigating to: '" + temp.url + "'.", MESSAGE_VERBOSE);
     if (err) throw err;
-    $ = cheerio.load(html);
-    temp.href = $("h2.header-version:contains(DOWNLOADS)").nextAll("p").find("a").attr("href");
-    temp.file = temp.href.substring(temp.href.lastIndexOf("/") + 1);
+    var $ = cheerio.load(html);
+    temp.url = $("h2.header-version:contains(DOWNLOADS)").nextAll("p").find("a").attr("href");
+	if(temp.url.includes('shrinkearn.com'))
+		temp.url = 'http://www.karyonix.net/shadersmod/files/ShadersModCore-v2.3.31-mc1.7.10-f.jar';
     updateFile(i, current, temp);
     if (!current.name) current.name = "Shaders Mod";
-    if (!("disabled" in current)) current.disabled = false;
 });
