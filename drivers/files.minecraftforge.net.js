409b82a28e9028f74cc64c1c897cf8addc81fe2a
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2019-01-04 22:55:55 +0100

    Download Forge Installer instead of the Universal file

--- drivers/files.minecraftforge.net.js
+++ drivers/files.minecraftforge.net.js
@@ -9,8 +9,7 @@ request(temp.url, function(err, response, html) {
 	console.message(i, "Navigating to: '" + temp.url + "'.", MESSAGE_VERBOSE);
 	if (err) throw err;
 	var $ = cheerio.load(html);
-	if (temp.preview === true) temp.url = $(".downloads>.download>.title>.promo-latest").parent().parent().find(".links .link .classifier-universal").parent().attr("href");
-	else temp.url = $(".downloads>.download>.title>.promo-recommended").parent().parent().find(".links .link .classifier-universal").parent().attr("href");
+	temp.url = $(".downloads>.download>.title>" + (temp.preview === true ? ".promo-latest" : ".promo-recommended")).parent().parent().find(".links .link .classifier-installer").parent().attr("href");
 	temp.url = temp.url.substring(temp.url.indexOf('&url=') + 5);
 	temp.file = temp.url.substring(temp.url.lastIndexOf("/") + 1);
 	updateFile(i, current, temp, callback);
