939740762bf017eb068df1597bcd082c96cf4664
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2019-01-06 10:09:01 +0100

    Unzip MultiMC

--- func/execCmd.js
+++ func/execCmd.js
@@ -20,8 +20,12 @@ module.exports = function(i, current) {
 	var exec = Object.assign({}, current.exec);
 
 	// Replace
-	exec.args = exec.args.replace('___JAVA___', '..\\bin\\windows\\x64\\JRE\\jre-8u152-windows-x64\\jre1.8.0_152\\bin\\java.exe');
-	exec.args = exec.args.replace('___FILE___', temp.file);
+	if(exec.args.includes("___7Z___"))
+		exec.args = exec.args.replace('___7Z___', '..\\bin\\windows\\x64\\7z\\7z.exe');
+	if(exec.args.includes("___JAVA___"))
+		exec.args = exec.args.replace('___JAVA___', '..\\bin\\windows\\x64\\JRE\\jre-8u152-windows-x64\\jre1.8.0_152\\bin\\java.exe');
+	if(exec.args.includes("___FILE___"))
+		exec.args = exec.args.replace('___FILE___', temp.file);
 
 	// Split arguments
 	if(exec.args && exec.args.length > 0)
