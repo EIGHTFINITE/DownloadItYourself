6faf4640b9803439b643946be9863fdd83aee91b
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2019-01-06 13:41:32 +0100

    Be more informative when verbosity is disabled

--- drivers/optifine.net.js
+++ drivers/optifine.net.js
@@ -2,14 +2,14 @@
  * Driver for downloading from optifine.net
  */
 request(temp.url, function(err, response, html) {
-    console.message(i, "Navigating to: '" + temp.url + "'.", MESSAGE_VERBOSE);
+    console.message(i, "Navigating to '" + temp.url + "'.");
     if (err) throw err;
     var $ = cheerio.load(html);
     var previewBuilds = $("span#preview td.downloadLineFile:contains(1.7.10)").nextAll("td.downloadLineMirror").children("a");
     if (temp.preview === true && previewBuilds.length > 0) temp.url = previewBuilds.attr("href");
     else temp.url = $("h2:contains(" + global.config["minecraft-version"] + ")").nextAll("table.downloadTable").first().find("td.downloadLineMirror a").attr("href");
     request(temp.url, function(err, response, html) {
-        console.message(i, "Navigating to: '" + temp.url + "'.", MESSAGE_VERBOSE);
+        console.message(i, "Navigating to '" + temp.url + "'.");
         if (err) throw err;
         $ = cheerio.load(html);
         temp.file = $("span#Download a").text().trim().replace(/^(Download )/, "").trim();
