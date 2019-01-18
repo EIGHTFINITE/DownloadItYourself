7c754b1ef6be1e7e3fb8c00ea1d7ac4a18abdefa
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2019-01-18 15:11:00 +0100

    Add AsieLib

--- drivers/files.vexatos.com.js
+++ drivers/files.vexatos.com.js
@@ -5,7 +5,7 @@ request(temp.url, function(err, response, html) {
 	console.message(i, "Navigating to '" + shortUrl(temp.url) + "'.");
 	if (err) throw err;
 	var $ = cheerio.load(html);
-	temp.url = response.request.uri.protocol + "//" + response.request.uri.host + response.request.uri.pathname + $('a[href*="' + global.config["minecraft-version"] + '"]:not([href$="-dev.jar"])').last().attr("href");
+	temp.url = response.request.uri.protocol + "//" + response.request.uri.host + "/" + $('a[href*="' + global.config["minecraft-version"] + '"]:not([href$="-deobf.jar"])').last().attr("href");
 	temp.file = temp.url.substring(temp.url.lastIndexOf("/") + 1);
 	updateFile(i, current, temp, callback);
 });
