2523acd9441ee19ac0ff7126cf17fe0e64f50770
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2019-01-19 16:22:18 +0100

    Add Sonic Ether's Unbelievable Shaders

---
+++ drivers/sonicether.com.js
@@ -0,0 +1,15 @@
+/**
+ * Driver for downloading from shadersmod.net
+ */
+request(temp.url, function(err, response, html) {
+	console.message(i, "Navigating to '" + shortUrl(temp.url) + "'.");
+	temp.url = "https://sonicether.com/shaders/download/v11.0/agree.php";
+	request({headers: {"Content-Type": "application/x-www-form-urlencoded"}, url: temp.url, form: {eula: 'accepted'}, method: 'POST'}, function(err, response, html) {
+		console.message(i, "Navigating to '" + shortUrl(temp.url) + "'.");
+		temp.url = "https://sonicether.com/shaders/download/v11.0/download.php";
+		temp.file = "SEUS-v11.0.zip";
+		temp.cookie = (response.headers['set-cookie'].join(';') + ';').match(/PHPSESSID=.*?;/).toString().slice(0,-1);
+		updateFile(i, current, temp, callback);
+	});
+	if (!current.name) current.name = "Sonic Ether's Unbelievable Shaders";
+});
