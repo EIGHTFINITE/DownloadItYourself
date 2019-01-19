7a36cd71010a5710af45620b3d2cabcdda731ac4
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2019-01-19 19:46:16 +0100

    Add KUDA-Shaders

---
+++ drivers/dedelner.net.js
@@ -0,0 +1,19 @@
+/**
+ * Driver for downloading from files.vexatos.com
+ */
+request(temp.url, function(err, response, html) {
+	console.message(i, "Navigating to '" + shortUrl(temp.url) + "'.");
+	if (err) throw err;
+	var $ = cheerio.load(html);
+	temp.url = $('a[href^="https://dedelner.net/download/"]').last().attr("href");
+	request(temp.url, function(err, response, html) {
+		console.message(i, "Navigating to '" + shortUrl(temp.url) + "'.");
+		if (err) throw err;
+		var $ = cheerio.load(html);
+		temp.url = $('a.wpdm-download-link').attr("onclick");
+		temp.url = temp.url.substring(temp.url.indexOf("'") + 1, nthIndex(temp.url, "'", 2));
+		temp.file = "KUDA-Shaders v6.5.56.zip";
+		updateFile(i, current, temp, callback);
+	});
+	if (!current.name) current.name = "KUDA-Shaders";
+});
