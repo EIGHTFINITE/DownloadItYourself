57fda533e8e022edfd21a38c03b03501931ec60c
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2019-01-23 23:44:35 +0100

    Configure Thaumcraft

--- bin/windows/all/scripts/multimc-setup.bat
+++ bin/windows/all/scripts/multimc-setup.bat
@@ -73,6 +73,9 @@ copy ..\docs\assets\config\mystcraft\core.cfg ..\minecraft\config\mystcraft\core
 :: Configure StarMiner
 copy ..\docs\assets\config\Starminer.cfg ..\minecraft\config\Starminer.cfg
 
+:: Configure Thaumcraft
+copy ..\docs\assets\config\Thaumcraft.cfg ..\minecraft\config\Thaumcraft.cfg
+
 :: Create shortcut
 if exist ..\Play Minecraft.lnk (
   del ..\Play Minecraft.lnk
