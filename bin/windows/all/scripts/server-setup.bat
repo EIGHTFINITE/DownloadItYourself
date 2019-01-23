47a33a196c1409ec28ec595d4ec53da2fb899282
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2019-01-23 09:34:07 +0100

    Configure Mystcraft

--- bin/windows/all/scripts/server-setup.bat
+++ bin/windows/all/scripts/server-setup.bat
@@ -34,7 +34,7 @@ echo ..\bin\windows\x64\JRE\jre-8u152-windows-x64\jre1.8.0_152\bin\java.exe -jar
 echo goto :EOF>>ServerStart.bat
 
 :: Create config directory
-mkdir config
+md config
 
 :: Configure Et Futurum
 echo general {>config\etfuturum.cfg
@@ -101,6 +101,22 @@ echo     S:OnSleepText=is now sleeping.>>config\Morpheus.cfg
 echo     S:OnWakeText=has left their bed.>>config\Morpheus.cfg
 echo     I:SleeperPerc=50>>config\Morpheus.cfg
 echo }>>config\Morpheus.cfg
+
+:: Configure Mystcraft
+md config\mystcraft
+echo baselining {>config\mystcraft\core.cfg
+echo     # If false, the profiling will run on game startup with the loading bar. If true, it will run in the background when playing. Setting this to false disables tickrate checking, even on the server.>>config\mystcraft\core.cfg
+echo     B:client.persave=true>>config\mystcraft\core.cfg
+echo.>>config\mystcraft\core.cfg
+echo     # If set to true this will prevent clients from connecting while baseline profiling is ongoing (Only works on dedicated servers)>>config\mystcraft\core.cfg
+echo     B:server.disconnectclients=false>>config\mystcraft\core.cfg
+echo.>>config\mystcraft\core.cfg
+echo     # This controls the minimum number of ticks to wait before a new chunk will be generated when doing the baseline profiling in the background.>>config\mystcraft\core.cfg
+echo     I:tickrate.minimum=^5>>config\mystcraft\core.cfg
+echo.>>config\mystcraft\core.cfg
+echo     # If true, the baseline calculations won't run and instead a config file will be read.>>config\mystcraft\core.cfg
+echo     B:useconfigs=true>>config\mystcraft\core.cfg
+echo }>>config\mystcraft\core.cfg
 
 :: Configure StarMiner
 echo basics_server_side_property {>config\Starminer.cfg
