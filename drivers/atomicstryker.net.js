14d74e143f5a882dd91cc4a4c1a2e94e6a643b58
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2019-02-03 09:38:04 +0100

    AdFly and Shrinkearn use fake domains
    These domains change from time to time. Let's just skip them.

--- drivers/atomicstryker.net.js
+++ drivers/atomicstryker.net.js
@@ -8,6 +8,11 @@ var elem = $('td:contains("' + global.config["minecraft-version"] + '")~td a').f
 if(!elem.length)
 	throw new Error("Couldn't find download link.");
 temp.url = elem.attr("href");
+// AdFly URLs have to be hardcoded
+if(temp.url === "http://adf.ly/q37Pu") {
+	console.message(i, "Navigating to '" + shortUrl(temp.url) + "'.");
+	temp.url = "https://raw.githubusercontent.com/AtomicStryker/github.io/master/files/1.7.10/BattleTowers-1.7.10.zip";
+}
 temp.file = temp.url.substring(temp.url.lastIndexOf("/") + 1).replace(/\?.*$/, "");
 updateFile(i, current, temp, callback);
 
