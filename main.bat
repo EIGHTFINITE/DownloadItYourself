0f584ac1ba13b587fccc4ee0c67c9735e22962ac
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2019-11-01 15:12:33 +0100

    Correct Windows path

--- main.bat
+++ main.bat
@@ -2,5 +2,5 @@
 setlocal
 cd /d %~dp0
 reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | "%windir%\System32\find.exe" /i "x86" > nul && set osArchitecture=86 || set osArchitecture=64
-cmd /c bin\windows\x%osArchitecture%\node\npm install
-bin\windows\x%osArchitecture%\node\node --use_strict main.js
+cmd /c bin\windows\x%osArchitecture%\node\node-v12.10.0-win-x%osArchitecture%\npm install
+bin\windows\x%osArchitecture%\node\node-v12.10.0-win-x%osArchitecture%\node --use_strict main.js
