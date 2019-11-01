64a969c86d0f613aad12806eee16aac2ce970bcc
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2019-11-01 13:54:41 +0100

    Add config script for Linux

---
+++ docs/scripts/configure-git.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+git config core.autocrlf false
+git config core.ignorecase false
+git config core.fscache true
+git config diff.renameLimit 0
+git config status.renameLimit 0
+git config merge.renameLimit 0
+git config http.postBuffer 1048576000
+git config pack.threads 1
+git config index.threads 0
