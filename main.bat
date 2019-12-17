21c8d78ddf1ee6b2d9715e353c15cde08995bc7e
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2019-12-17 13:19:23 +0100

    Do not run npm before starting node
    This is unnecessary since we already vendor the node modules

--- main.bat
+++ main.bat
@@ -2,5 +2,4 @@
 setlocal
 cd /d %~dp0
 reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | "%windir%\System32\find.exe" /i "x86" > nul && set osArchitecture=86 || set osArchitecture=64
-cmd /c bin\windows\x%osArchitecture%\node\node-v12.10.0-win-x%osArchitecture%\npm install --no-optional --no-bin-links --ignore-scripts --loglevel=error
 bin\windows\x%osArchitecture%\node\node-v12.10.0-win-x%osArchitecture%\node --use_strict main.js
