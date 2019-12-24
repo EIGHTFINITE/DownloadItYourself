4e5b4b0b447a00bed3e2f2ffb8eb952cf236e95b
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2019-12-24 16:02:30 +0100

    [WIP] Generate readme

--- drivers/sonicether.com.js
+++ drivers/sonicether.com.js
@@ -4,21 +4,21 @@
 (function() {
 
 temp.url = "https://sonicether.com/shaders/download/v11.0/agree.php";
-request({headers: {"Content-Type": "application/x-www-form-urlencoded"}, url: temp.url, form: {eula: 'accepted'}, method: 'POST'}, function(err, response, html) {
+cloudscraper({headers: {"Content-Type": "application/x-www-form-urlencoded"}, url: temp.url, form: {eula: 'accepted'}, method: 'POST'}, function(err, response, html) {
 	// Connection error
 	if(err) {
-		console.message(i, "WARNING: '" + shortUrl(remote.href) + "' failed with " + err.code + ". Retrying…");
+		console.log("WARNING: '" + shortUrl(remote.href) + "' failed with " + err.code + ". Retrying…");
 		updateFile(i, current, temp, callback);
 		return;
 	}
 	// Server error
 	if(response.statusCode !== 200) {
-		console.message(i, "WARNING: '" + shortUrl(remote.href) + "' failed with " + response.statusCode + ". Retrying…");
+		console.log("WARNING: '" + shortUrl(remote.href) + "' failed with " + response.statusCode + ". Retrying…");
 		updateFile(i, current, temp, callback);
 		return;
 	}
 
-	console.message(i, "Navigating to '" + shortUrl(temp.url) + "'.");
+	console.log("Navigating to '" + shortUrl(temp.url) + "'.");
 	temp.url = "https://sonicether.com/shaders/download/v11.0/download.php";
 	temp.file = "SEUS-v11.0.zip";
 	temp.cookie = (response.headers['set-cookie'].join(';') + ';').match(/PHPSESSID=.*?;/).toString().slice(0,-1);
--- func/updateFile.js
+++ func/updateFile.js
@@ -1,27 +1,34 @@
 (function() {
 
 // Libraries
+var is = require('@sindresorhus/is');
 var fs = require("fs-extra");
 var url = require("url");
 var cheerio = require("cheerio");
-var request = require("request");
 var md5File = require("md5-file");
+var waitUntil = require("wait-until");
+var spawn = require("child_process").spawn;
 
 // Functions
 var localizedName = require("../func/localizedName.js");
-var closeThread = require("../func/closeThread.js");
 var shortUrl = require("../func/shortUrl.js");
 var nthIndex = require("../func/nthIndex.js");
 
 // Variables
 var MESSAGE_VERBOSE = true;
+var MESSAGE_NOT_VERBOSE = false;
+var CLOSE_THREAD = true;
+var NO_CLOSE_THREAD = false;
 
 module.exports = function(i, current, temp, callback) {
 	var updateFile = module.exports;
 
-	if(!current.url) {
-		console.message(i, "WARNING: '" + localizedName(i) + "' has no URL. It will not be updated.");
-		callback();
+	if(!current.url || current.url.startsWith('https://www.curseforge.com/')) { // TODO: Get CurseForge working again
+		if(console.message) {
+			console.message(i, "'" + localizedName(i) + "' will not be updated automatically.", MESSAGE_NOT_VERBOSE, global.noExecOrUpdateIsFinished[i] === true ? CLOSE_THREAD : NO_CLOSE_THREAD);
+			global.noExecOrUpdateIsFinished[i] = true;
+		}
+		callback(current);
 		return;
 	}
 
@@ -30,62 +37,115 @@ module.exports = function(i, current, temp, callback) {
 		throw new Error("Unknown protocol.");
 
 	// Copy current into temp
-	if (typeof temp === 'undefined') {
+	if (is.undefined(temp)) {
 		temp = Object.assign({}, current);
 		delete temp.file;
 		delete temp.md5;
-		console.message(i, "Checking '" + localizedName(i) + "' for updates.");
+		if(console.message)
+			console.message(i, "Checking '" + localizedName(i) + "' for updates.");
+		else
+			console.log("Checking '" + localizedName(i) + "' for updates.")
 	}
 
-	// Read the headers of our next url
-	request(temp.cookie ? {headers: {"Cookie": temp.cookie}, url: temp.url} : temp.url, function(err, response, html){
-		var remote = this;
-		// Connection error
-		if(err) {
-			console.message(i, "WARNING: '" + shortUrl(remote.href) + "' failed with error '" + err.code + "'. Retrying…");
-			updateFile(i, current, temp, callback);
-			return;
-		}
-		// Server error
-		if(response.statusCode !== 200) {
-			console.message(i, "WARNING: '" + shortUrl(remote.href) + "' failed with error '" + response.statusCode + "'. Retrying…");
-			updateFile(i, current, temp, callback);
-			return;
-		}
-		// If URL is a webpage
-		if(response.headers['content-type'].startsWith('text/html')) {
-			if(!fs.existsSync("../drivers/" + remote.host + ".js"))
-				throw new Error("Missing driver for '" + remote.host + "'. URL was '" + remote.href + "'");
-			var script = fs.readFileSync("../drivers/" + remote.host + ".js", "utf8");
-			console.message(i, "Navigating to '" + shortUrl(temp.url) + "'.");
-			try {
-				eval(script);
-			} catch(er) {
-				if(er.toString() === "Error: Couldn't find download link.") {
-					console.message(i, "WARNING: '" + shortUrl(remote.href) + "' failed with error \"Couldn't find download link.\" Retrying…");
-					updateFile(i, current, temp, callback);
-					return;
+	// If we're not already a child process
+	if(global.threads) {
+		// Wait until the previous files have been updated
+		waitUntil(1000, Infinity, function() {
+			var counter = 0;
+			for(var j = i - 1; j >= 0; j--) {
+				if(global.threads[j] !== false) { // If the thread hasn't been closed
+					if(current.url.startsWith('https://www.curseforge.com/') && global.downloads[j].url.startsWith('https://www.curseforge.com/')) // Never have more than one opened CurseForge thread
+						return false;
+					counter++; // Then increase the counter
+					if(counter >= global.config["max-active-downloads"]) // If too many threads are still open
+						return false; // Then keep waiting
 				}
-				throw er;
 			}
+			return true;
+		}, function() {
+			// Slow down CurseForge requests
+			if(current.url.startsWith('https://www.curseforge.com/')) {
+				setTimeout(function () {
+					spawnChild();
+				}, 5000);
+			}
+			else {
+				spawnChild();
+			}
+		});
+	}
+	else {
+		// Slow down CurseForge requests
+		if(current.url.startsWith('https://www.curseforge.com/')) {
+			setTimeout(function () {
+				spawnChild();
+			}, 3000);
 		}
-	}).on('response', function(response) {
-		var remote = this;
+		else {
+			// Otherwise create a child immediately
+			spawnChild();
+		}
+	}
 
-		// If URL is a file
-		if(!response.headers['content-type'].startsWith('text/html')) {
-			if(!("file" in temp))
-				temp.file = temp.url.substring(temp.url.lastIndexOf("/") + 1).replace(/\?.*$/, "");
-			var shortenedUrl = shortUrl(remote.href, temp.file);
-			console.message(i, "Downloading '" + shortenedUrl + (shortenedUrl.substring(shortenedUrl.lastIndexOf("/") + 1) === temp.file ? "" : "' as '" + temp.file) + "'.");
-			var script = fs.readFileSync("../drivers/default.js", "utf8");
-			try {
-				eval(script);
-			} catch(er) {
-				throw er;
+	// Spawn child process
+	function spawnChild() {
+		var child = spawn(process.execPath, ["--use_strict", "--no-warnings", __dirname + "/updateFileChild.js", JSON.stringify(i), JSON.stringify(current), JSON.stringify(temp)]);
+
+		child.on('exit', function(code){
+			clearTimeout(to);
+			if(code === 256) {
+				if(console.message)
+					console.message(i, "Child process finished successfully.", MESSAGE_VERBOSE);
+				callback(current);
+				return;
 			}
-		}
-	});
+			if(console.message)
+				console.message(i, "Child process failed with error code " + code + ". Retrying…");
+			else
+				console.log("Child process failed with error code " + code + ". Retrying…");
+			spawnChild();
+		});
+
+		child.stdout.on('data', function (data) {
+			// Split text if we recieved more than one message
+			var dataSplit = String(data).trim().match(/[^\r\n]+/g);
+			var dataCurrent;
+			// Process messages
+			for(var j = 0; j < dataSplit.length; j++) {
+				dataCurrent = dataSplit[j].trim();
+				if(dataCurrent.startsWith('VariableAlteration$current:')) {
+					// Merge the variables
+					current = {...current, ...JSON.parse(dataCurrent.substring(27))};
+				}
+				else if(console.message) {
+					if(dataCurrent.startsWith("'" + localizedName(i) + "' has successfully updated.") || dataCurrent.startsWith("'" + localizedName(i) + "' has already been updated to the latest version.")) {
+						console.message(i, dataCurrent, MESSAGE_NOT_VERBOSE, global.noExecOrUpdateIsFinished[i] === true ? CLOSE_THREAD : NO_CLOSE_THREAD);
+						global.noExecOrUpdateIsFinished[i] = true;
+					}
+					else
+						console.message(i, dataCurrent);
+				}
+				else
+					console.log(dataCurrent);
+			}
+		});
+
+		child.stderr.on('data', function (data) {
+			if(console.message)
+				console.message(i, "Child process failed with error:\r\n " + data);
+			else
+				throw new Error("Error on thread " + i + ":\r\n" + data);
+		});
+
+		var to = setTimeout(function(){
+			child.kill();
+			if(console.message)
+				console.message(i, "Child process timed out. Retrying…");
+			else
+				console.log("Child process timed out. Retrying…");
+			spawnChild();
+		}, 600000);
+	}
 }
 
 })();
