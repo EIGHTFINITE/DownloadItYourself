efeffe92cf41935db94352b9eba518f08ef1b9e3
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2020-05-27 13:51:34 +0200

    Verify Node version

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
 (function() {
 
-// Executable check
-if(require('is-iojs'))
-	throw Error("EIGHTFINITE-build does not support io.js");
-
 // Libraries
 var fs = require("fs-extra");
 var stringify = require("json-stable-stringify");
@@ -19,6 +15,10 @@ console.message = require("./src/message.js");
 // Ensure correct working directory
 fs.ensureDirSync(__dirname + "/_download");
 process.chdir(__dirname + "/_download");
+
+// Executable check
+if(process.versions.node !== JSON.parse(fs.readFileSync("../package.json", "utf8")).engines.node)
+	throw Error("EIGHTFINITE-build only supports Node " + process.versions.node);
 
 // Globals
 global.list = void(0);
