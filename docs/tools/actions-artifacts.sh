97b5c1d434c2b2c15310f2ca5ada906177f1189d
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2021-12-25 14:57:20 +0100

    Adjust the location of tslib

--- docs/tools/actions-artifacts.sh
+++ docs/tools/actions-artifacts.sh
@@ -297,7 +297,7 @@ rm -r node_modules/npm-6/node_modules/npm/node_modules/wrap-ansi/node_modules/an
 rm -r node_modules/npm-6/node_modules/npm/node_modules/yargs/node_modules/ansi-regex/
 rm -r node_modules/npm/node_modules/json-schema/
 rm -r node_modules/npm-6/node_modules/npm/node_modules/json-schema/
-rm node_modules/cheerio/node_modules/tslib/modules/package.json
+rm node_modules/tslib/modules/package.json
 rm node_modules/electron/install.js
 cp -a node_modules/ansi-regex/ node_modules/npm/node_modules/cli-table3/node_modules/ansi-regex/
 cp -a node_modules/ansi-regex/ node_modules/npm/node_modules/string-width/node_modules/ansi-regex/
