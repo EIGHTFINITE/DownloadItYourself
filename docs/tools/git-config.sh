c93d1694d2ec15fcdddf7ed79793aec0a69fd3c8
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2021-12-27 15:43:20 +0100

    Increase waiting time before aborting

--- docs/tools/git-config.sh
+++ docs/tools/git-config.sh
@@ -6,6 +6,8 @@ git config core.longpaths true
 git config diff.renameLimit 0
 git config status.renameLimit 0
 git config merge.renameLimit 0
+git config http.lowSpeedLimit 0
+git config http.lowSpeedTime 300
 git config http.postBuffer 1048576000
 git config pack.threads 1
 git config index.threads 0
