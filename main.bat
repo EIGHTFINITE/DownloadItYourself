5ae7c4605984bd37a7ff5daa165537d28af75266
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2022-06-01 10:54:12 +0200

    Shorten variable

--- main.bat
+++ main.bat
@@ -1,5 +1,5 @@
 @echo off
 setlocal
 cd /d %~dp0
-reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | "%windir%\System32\find.exe" /i "x86" > nul && set osArchitecture=86 || set osArchitecture=64
-cmd /c bin\windows\x%osArchitecture%\node\node-v12.22.12-win-x%osArchitecture%\node --use_strict main.js
+reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | "%windir%\System32\find.exe" /i "x86" > nul && set osArch=86 || set osArch=64
+cmd /c bin\windows\x%osArch%\node\node-v12.22.12-win-x%osArch%\node --use_strict main.js
