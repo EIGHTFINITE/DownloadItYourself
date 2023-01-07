e3feae554b4c132babec589a2359590bab2ecd5e
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2023-01-07 19:37:39 +0100

    Update User Agent

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 (function() {
 
 // Version
-var nodeVersion = require('./package.json').engines.node;
-var electronVersion = require('./node_modules/electron/package.json').version;
+const nodeVersion = require('./package.json').engines.node;
+const electronVersion = require('./node_modules/electron/package.json').version;
+let electronUserAgent = require('./node_modules/top-user-agents/index.json')[0];
 
 // Env
 process.env.ELECTRON_OVERRIDE_DIST_PATH = (/^win/.test(process.platform) ? 'bin/windows/x64/electron/electron-v' + electronVersion + '-win32-x64' : 'bin/linux/x64/electron/electron-v' + electronVersion + '-linux-x64');
@@ -25,6 +26,7 @@ else {
 	if(process.versions.electron !== electronVersion) {
 		throw Error('Expected Electron ' + electronVersion + ' instead of Electron ' + process.versions.electron);
 	}
+	console.log('User Agent set to "' + electronUserAgent + '"');
 
 	// Require
 	const fs = require('fs');
@@ -43,18 +45,24 @@ else {
 			}
 		});
 
-		win.loadURL('https://techblog.willshouse.com/2012/01/03/most-common-user-agents/', { userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'});
+		win.loadURL('https://techblog.willshouse.com/2012/01/03/most-common-user-agents/', {userAgent: electronUserAgent});
 
 		// Intentionally using on instead of once
 		win.webContents.on('did-finish-load', () => {
 			win.webContents.executeJavaScript(`[...document.querySelectorAll('tbody .useragent')].map(e => e.innerText)`).then((result) => {
-				console.log(result);
 				if(result.length > 0) {
-					fs.writeFileSync('./index.json', stringify(result), 'utf-8');
+					// Update User Agent
+					if(electronUserAgent !== result[0]) {
+						electronUserAgent = result[0];
+						console.log('User Agent updated to "' + electronUserAgent + '"');
+					}
+					// Write User Agents to file
+					fs.writeFileSync('./node_modules/top-user-agents/index.json', stringify(result), 'utf-8');
 					app.exit();
 				}
 			}).catch((error) => {
 				console.log(error);
+				app.exit();
 			});
 		});
 	});
