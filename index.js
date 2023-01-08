2998cd3cc0e8bd1d918c75f32324f74554a5ba1a
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2023-01-08 17:35:14 +0100

    Code style

--- index.js
+++ index.js
@@ -2,42 +2,42 @@
 'use strict'
 
 // Version
-const nodeVersion = require('./package.json').engines.node;
-const electronVersion = require('./node_modules/electron/package.json').version;
+const nodeVersion = require('./package.json').engines.node
+const electronVersion = require('./node_modules/electron/package.json').version
 
 // Env
-process.env.ELECTRON_OVERRIDE_DIST_PATH = (/^win/.test(process.platform) ? 'bin/windows/x64/electron/electron-v' + electronVersion + '-win32-x64' : 'bin/linux/x64/electron/electron-v' + electronVersion + '-linux-x64');
+process.env.ELECTRON_OVERRIDE_DIST_PATH = (/^win/.test(process.platform) ? 'bin/windows/x64/electron/electron-v' + electronVersion + '-win32-x64' : 'bin/linux/x64/electron/electron-v' + electronVersion + '-linux-x64')
 
 // Executable check
 if(process.versions.electron) {
-	console.log('Running on Electron ' + process.versions.electron + ' + Node ' + process.versions.node);
+	console.log('Running on Electron ' + process.versions.electron + ' + Node ' + process.versions.node)
 	if(process.versions.electron !== electronVersion) {
-		throw Error('Expected Electron ' + electronVersion + ' instead of Electron ' + process.versions.electron);
+		throw Error('Expected Electron ' + electronVersion + ' instead of Electron ' + process.versions.electron)
 	}
 
 	// Require
-	const fs = require('fs');
-	const { app, BrowserWindow } = require('electron');
-	const stringify = require('./node_modules/npm/node_modules/json-stringify-nice');
-	const userAgents = require('top-user-agents');
-	let electronUserAgent = userAgents[0];
-	console.log('User Agent set to "' + electronUserAgent + '"');
+	const fs = require('fs')
+	const { app, BrowserWindow } = require('electron')
+	const stringify = require('./node_modules/npm/node_modules/json-stringify-nice')
+	const userAgents = require('top-user-agents')
+	let electronUserAgent = userAgents[0]
+	console.log('User Agent set to "' + electronUserAgent + '"')
 
 	// Function
 	function isArrayLike(a) {
-		return (Array.isArray(a) || (a !== null && typeof a === "object" && typeof a.length === "number" && (a.length === 0 || (a.length > 0 && (a.length - 1) in a))));
+		return (Array.isArray(a) || (a !== null && typeof a === "object" && typeof a.length === "number" && (a.length === 0 || (a.length > 0 && (a.length - 1) in a))))
 	}
 	function isEqualArrayShallow(a, b) {
 		if (!isArrayLike(a) || !isArrayLike(b) || a.length !== b.length) {
-			return false;
+			return false
 		}
-		let i=0;
+		let i=0
 		for (; i<a.length; i++) {
 			if (a[i] !== b[i]) {
-				return false;
+				return false
 			}
 		}
-		return true;
+		return true
 	}
 
 	// Run
@@ -50,9 +50,9 @@ if(process.versions.electron) {
 			webPreferences: {
 				backgroundThrottling: false
 			}
-		});
+		})
 
-		win.loadURL('https://techblog.willshouse.com/2012/01/03/most-common-user-agents/', {userAgent: electronUserAgent});
+		win.loadURL('https://techblog.willshouse.com/2012/01/03/most-common-user-agents/', {userAgent: electronUserAgent})
 
 		// Intentionally using on instead of once
 		win.webContents.on('did-finish-load', () => {
@@ -60,36 +60,36 @@ if(process.versions.electron) {
 				if(result.length > 0) {
 					// Update User Agent
 					if(electronUserAgent !== result[0]) {
-						electronUserAgent = result[0];
-						console.log('User Agent updated to "' + electronUserAgent + '"');
+						electronUserAgent = result[0]
+						console.log('User Agent updated to "' + electronUserAgent + '"')
 					}
 					// Write User Agents to file
 					if(!isEqualArrayShallow(userAgents, result)) {
-						fs.writeFileSync('./node_modules/top-user-agents/index.json', stringify(result), 'utf-8');
+						fs.writeFileSync('./node_modules/top-user-agents/index.json', stringify(result), 'utf-8')
 					}
-					app.exit();
+					app.exit()
 				}
 			}).catch((error) => {
-				console.error(error);
-				app.exit();
-			});
-		});
-	});
+				console.error(error)
+				app.exit()
+			})
+		})
+	})
 }
 else {
-	console.log('Running on Node ' + process.versions.node);
+	console.log('Running on Node ' + process.versions.node)
 	if(process.versions.node !== nodeVersion) {
-		throw Error('Expected Node ' + nodeVersion + ' instead of Node ' + process.versions.node);
+		throw Error('Expected Node ' + nodeVersion + ' instead of Node ' + process.versions.node)
 	}
 
 	// Start Electron
-	const electron = require('electron');
-	const { spawn } = require('child_process');
-	console.log('Starting Electron ' + electronVersion);
-	spawn(electron, ['--use_strict', 'index.js'], { stdio: 'inherit' });
+	const electron = require('electron')
+	const { spawn } = require('child_process')
+	console.log('Starting Electron ' + electronVersion)
+	spawn(electron, ['--use_strict', 'index.js'], { stdio: 'inherit' })
 }
 
 // After everything is done (even if we error out)
 process.on('exit', () => { // Asynchronous functions do not work beyond this point
-	console.log(process.versions.electron ? 'Exiting Electron ' + process.versions.electron + ' + Node ' + process.versions.node : 'Exiting Node ' + process.versions.node);
-});
+	console.log(process.versions.electron ? 'Exiting Electron ' + process.versions.electron + ' + Node ' + process.versions.node : 'Exiting Node ' + process.versions.node)
+})
