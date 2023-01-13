0d571df3b84ee21acff65b473d0a5a25315299d7
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2023-01-13 09:05:47 +0100

    Escape properly

--- index.js
+++ index.js
@@ -7,158 +7,161 @@ const fs = require('./node_modules/npm/node_modules/graceful-fs')
 
 // Function
 function writeReadme() {
-	function traverseDependencies(d,p) {
-		// Require
-		const isString = require('./node_modules/lodash.isstring')
-		
-		let a = []
-		Object.keys(d).forEach((k) => {
-			// Name
-			d[k].name = k
+	function generateReadme(t) {
+		function traverseDependencies(d,p) {
+			// Require
+			const isString = require('./node_modules/lodash.isstring')
 			
-			// Path
-			d[k].path = (isString(p) ? p + '/node_modules/' + k : 'node_modules/' + k)
-			
-			// Location
-			d[k].location = d[k].path.replaceAll('node_modules/','')
-			
-			// Package
-			const pkg = require('./' + d[k].path + '/package.json')
-			
-			// Version
-			d[k].version = pkg.version
-			
-			// License
-			if(!pkg.license) {
-				d[k].license = ''
-			}
-			else if(isString(pkg.license)) {
-				d[k].license = pkg.license
-			}
-			else {
-				d[k].license = pkg.license.type
-			}
-			
-			// Author
-			if(!pkg.author) {
-				d[k].author = ''
-			}
-			else if(isString(pkg.author)) {
-				d[k].author = pkg.author
-			}
-			else {
-				d[k].author = pkg.author.name
-			}
-			
-			// Source Code
-			if(!pkg.repository) {
-				d[k].source = ''
-			}
-			else if(isString(pkg.repository)) {
-				d[k].source = pkg.repository
-			}
-			else {
-				d[k].source = pkg.repository.url
-			}
-			if(d[k].source.startsWith('git+')) {
-				d[k].source = d[k].source.slice(4)
-			}
-			if(d[k].source.startsWith('git://')) {
-				d[k].source = 'https://' + d[k].source.slice(6)
-			}
-			else if(d[k].source.startsWith('ssh://git@')) {
-				d[k].source = 'https://' + d[k].source.slice(10)
-			}
-			if(d[k].source.endsWith('.git')) {
-				d[k].source = d[k].source.slice(0,-4)
-			}
-			
-			// Description
-			const _requiredBy = pkg._requiredBy
-			if(isString(pkg.description)) {
-				d[k].description = pkg.description.replace(/[^ -~]/g,' ').trim().replace(/ +/g,' ').replace(/[Nn]ode\.js|[Nn]ode(?!s)/g,'Node').replace(/\.$/,'') + '.'
-				d[k].description = d[k].description[0].toUpperCase() + d[k].description.slice(1)
-			}
-			else {
-				d[k].description = ''
-			}
-			for (let i=0; i<_requiredBy.length; i++) {
-				if(_requiredBy[i] === '/') {
-					_requiredBy.splice(i, 1)
-					i--
-					continue
+			let a = []
+			Object.keys(d).forEach((k) => {
+				// Name
+				d[k].name = k
+				
+				// Path
+				d[k].path = (isString(p) ? p + '/node_modules/' + k : 'node_modules/' + k)
+				
+				// Location
+				d[k].location = d[k].path.replaceAll('node_modules/','')
+				
+				// Package
+				const pkg = require('./' + d[k].path + '/package.json')
+				
+				// Version
+				d[k].version = pkg.version
+				
+				// License
+				if(!pkg.license) {
+					d[k].license = ''
 				}
-				if(i === 0) {
-					if(d[k].description === '') {
-						d[k].description += 'Required by '
-					}
-					else {
-						d[k].description += '<br>Required by '
-					}
+				else if(isString(pkg.license)) {
+					d[k].license = pkg.license
 				}
 				else {
-					d[k].description += ', '
+					d[k].license = pkg.license.type
 				}
-				d[k].description += _requiredBy[i].slice(1)
-				if(i === _requiredBy.length-1) {
-					d[k].description += '.'
+				
+				// Author
+				if(!pkg.author) {
+					d[k].author = ''
 				}
-			}
-			
-			// Add to array
-			a.push(d[k])
-			
-			// Resolve children
-			if(d[k].dependencies) {
-				const b = traverseDependencies(d[k].dependencies,d[k].path)
-				for (let i=0; i<b.length; i++) {
-					a.push(b[i])
+				else if(isString(pkg.author)) {
+					d[k].author = pkg.author
 				}
-			}
-		})
-		return a
-	}
-	function htmlspecialchars(s) {
-		return s.replaceAll('&', '&amp;').replaceAll('"', '&quot;').replaceAll("'", '&#039;').replaceAll('<', '&lt;').replaceAll('>', '&gt;')
-	}
-	
-	// Readme
-	const dependencies = traverseDependencies(require('./package-lock.json').dependencies)
-	let html = '<h2>Node dependencies</h2>\n'
-	html += '<table>\n'
-	html += '<tr>\n'
-	html += '<th>Icon</th>\n'
-	html += '<th>Name</th>\n'
-	html += '<th>Author</th>\n'
-	html += '<th>License</th>\n'
-	html += '<th>Source&nbsp;Code</th>\n'
-	html += '<th>Distribution</th>\n'
-	html += '<th>Description</th>\n'
-	html += '<th>Version</th>\n'
-	html += '</tr>\n'
-	for (let i=0; i<dependencies.length; i++) {
-		const d = dependencies[i]
-		const escName = htmlspecialchars(d.name)
-		const escLocation = htmlspecialchars(d.location)
-		const escAuthor = htmlspecialchars(d.author)
-		const escLicense = htmlspecialchars(d.license)
-		const escSource = htmlspecialchars(d.source)
-		const escDescription = htmlspecialchars(d.description)
-		const escVersion = htmlspecialchars(d.version)
+				else {
+					d[k].author = pkg.author.name
+				}
+				
+				// Source Code
+				if(!pkg.repository) {
+					d[k].source = ''
+				}
+				else if(isString(pkg.repository)) {
+					d[k].source = pkg.repository
+				}
+				else {
+					d[k].source = pkg.repository.url
+				}
+				if(d[k].source.startsWith('git+')) {
+					d[k].source = d[k].source.slice(4)
+				}
+				if(d[k].source.startsWith('git://')) {
+					d[k].source = 'https://' + d[k].source.slice(6)
+				}
+				else if(d[k].source.startsWith('ssh://git@')) {
+					d[k].source = 'https://' + d[k].source.slice(10)
+				}
+				if(d[k].source.endsWith('.git')) {
+					d[k].source = d[k].source.slice(0,-4)
+				}
+				
+				// Description
+				if(isString(pkg.description)) {
+					d[k].description = pkg.description.replace(/[^ -~]/g,' ').trim().replace(/ +/g,' ').replace(/[Nn]ode\.js|[Nn]ode(?!s)/g,'Node').replace(/\.$/,'') + '.'
+					d[k].description = d[k].description[0].toUpperCase() + d[k].description.slice(1)
+				}
+				else {
+					d[k].description = ''
+				}
+				
+				// Required by
+				d[k].requiredBy = ''
+				for (let i=0; i<pkg._requiredBy.length; i++) {
+					if(pkg._requiredBy[i] === '/') {
+						pkg._requiredBy.splice(i, 1)
+						i--
+						continue
+					}
+					if(i === 0) {
+						d[k].requiredBy += 'Required by '
+					}
+					else {
+						d[k].requiredBy += ', '
+					}
+					d[k].requiredBy += pkg._requiredBy[i].slice(1)
+					if(i === pkg._requiredBy.length-1) {
+						d[k].requiredBy += '.'
+					}
+				}
+				
+				// Add to array
+				a.push(d[k])
+				
+				// Resolve children
+				if(d[k].dependencies) {
+					const b = traverseDependencies(d[k].dependencies,d[k].path)
+					for (let i=0; i<b.length; i++) {
+						a.push(b[i])
+					}
+				}
+			})
+			return a
+		}
+		function htmlspecialchars(s) {
+			return s.replaceAll('&', '&amp;').replaceAll('"', '&quot;').replaceAll("'", '&#039;').replaceAll('<', '&lt;').replaceAll('>', '&gt;')
+		}
+		
+		// Readme
+		const dependencies = traverseDependencies(require('./package-lock.json').dependencies)
+		let html = '<h2>Node dependencies</h2>\n'
+		html += '<table>\n'
 		html += '<tr>\n'
-		html += '<td align="center"><a href="https://www.npmjs.com/package/' + escName +'" title="' + escName +'"><img src="https://raw.githubusercontent.com/npm/logos/master/npm%20logo/classic/npm-2009.svg" width="31" alt="' + escName +'" title="' + escName +'"></a></td>\n'
-		html += '<td><a href="https://www.npmjs.com/package/' + escName +'" title="' + escName +'">' + escLocation + '</a></td>\n'
-		html += '<td>' + escAuthor + '</td>\n'
-		html += '<td>' + escLicense + '</td>\n'
-		html += '<td>' + escSource + '</td>\n'
-		html += '<td align="center"><a href="##" title="Distribution Allowed"><img src="docs/img/svg/check.svg" width="24" alt="OK" title="Distribution Allowed"></a></td>\n'
-		html += '<td>' + escDescription +'</td>\n'
-		html += '<td align="center">' + escVersion + '</td>\n'
+		html += '<th>Icon</th>\n'
+		html += '<th>Name</th>\n'
+		html += '<th>Author</th>\n'
+		html += '<th>License</th>\n'
+		html += '<th>Source&nbsp;Code</th>\n'
+		html += '<th>Distribution</th>\n'
+		html += '<th>Description</th>\n'
+		html += '<th>Version</th>\n'
 		html += '</tr>\n'
+		for (let i=0; i<dependencies.length; i++) {
+			const d = dependencies[i]
+			if(t === 'md' && (d.location.startsWith('npm/') || d.location.startsWith('npm-6/'))) {
+				continue
+			}
+			const escName = htmlspecialchars(d.name)
+			const escLocation = htmlspecialchars(d.location)
+			const escAuthor = htmlspecialchars(d.author)
+			const escLicense = htmlspecialchars(d.license)
+			const escSource = htmlspecialchars(d.source)
+			const escDescription = htmlspecialchars(d.description)
+			const escRequiredBy = htmlspecialchars(d.requiredBy)
+			const escVersion = htmlspecialchars(d.version)
+			html += '<tr>\n'
+			html += '<td align="center"><a href="https://www.npmjs.com/package/' + escName +'" title="' + escName +'"><img src="https://raw.githubusercontent.com/npm/logos/master/npm%20logo/classic/npm-2009.svg" width="31" alt="' + escName +'" title="' + escName +'"></a></td>\n'
+			html += '<td><a href="https://www.npmjs.com/package/' + escName +'" title="' + escName +'">' + escLocation + '</a></td>\n'
+			html += '<td>' + escAuthor + '</td>\n'
+			html += '<td>' + escLicense + '</td>\n'
+			html += '<td>' + escSource + '</td>\n'
+			html += '<td align="center"><a href="##" title="Distribution Allowed"><img src="docs/img/svg/check.svg" width="24" alt="OK" title="Distribution Allowed"></a></td>\n'
+			html += '<td>' + (escDescription !== '' ? (escRequiredBy !== '' ? escDescription + '<br>' + escRequiredBy : escDescription) : escRequiredBy) +'</td>\n'
+			html += '<td align="center">' + escVersion + '</td>\n'
+			html += '</tr>\n'
+		}
+		return html += '</table>\n'
 	}
-	html += '</table>\n'
-	fs.writeFileSync('README.html', html, 'utf-8')
-	fs.writeFileSync('README.md', html, 'utf-8')
+	fs.writeFileSync('README.html', generateReadme('html'), 'utf-8')
+	fs.writeFileSync('README.md', generateReadme('md'), 'utf-8')
 }
 
 // Executable check
