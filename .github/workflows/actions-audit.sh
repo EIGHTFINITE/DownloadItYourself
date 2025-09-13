6c2d2d892663fc46f50ced1cc5a1336076d60119
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2025-09-13 21:39:45 +0200

    Also audit both versions of npm

--- .github/workflows/actions-audit.sh
+++ .github/workflows/actions-audit.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 export node_version=$(cat package.json | python -c "import sys, json; print(json.load(sys.stdin)['engines']['node'])")
+export npm_version=$(cat bin/linux/x64/node/node-v$node_version-linux-x64/lib/node_modules/npm/package.json | python -c "import sys, json; print(json.load(sys.stdin)['version'])")
+new_npm_version=$(cat bin/windows/x64/node/node-v$node_version-win-x64/node_modules/npm/package.json | python -c "import sys, json; print(json.load(sys.stdin)['version'])")
 # Swap npm versions
 # Keeping this swap in actions means we can audit freely on real Windows machines
 mv -T "bin/linux/x64/node/node-v$node_version-linux-x64/lib/node_modules/npm" "bin/linux/x64/node/node-v$node_version-linux-x64/lib/node_modules/npm-temp"
@@ -16,13 +18,13 @@ chmod +x bin/linux/x64/node/node-v$node_version-linux-x64/bin/node
 bin/linux/x64/node/node-v$node_version-linux-x64/bin/node node_modules/better-npm-audit/index.js audit
 export PATH="$actions_PATH"
 rm -rf .npm/
-git checkout -- '.npmrc'
-export better_npm_audit_version=$(cat package.json | python -c "import sys, json; print(json.load(sys.stdin)['dependencies']['better-npm-audit'])")
-export electron_version=$(cat package.json | python -c "import sys, json; print(json.load(sys.stdin)['devDependencies']['electron'])")
-export lodash_isarray_version=$(cat package.json | python -c "import sys, json; print(json.load(sys.stdin)['optionalDependencies']['lodash.isarray'])")
-# devDependencies and optionalDependencies
 rm -r node_modules/
 rm package-lock.json
+git checkout -- '.npmrc'
+# other dependencies
+better_npm_audit_version=$(cat package.json | python -c "import sys, json; print(json.load(sys.stdin)['dependencies']['better-npm-audit'])")
+electron_version=$(cat package.json | python -c "import sys, json; print(json.load(sys.stdin)['devDependencies']['electron'])")
+lodash_isarray_version=$(cat package.json | python -c "import sys, json; print(json.load(sys.stdin)['optionalDependencies']['lodash.isarray'])")
 # Ignore dependencies, devDependencies, optionalDependencies, peerDependencies, and bundleDependencies
 sed -i '/"dependencies": {/,/}/d' -- 'package.json'
 sed -i '/"devDependencies": {/,/}/d' -- 'package.json'
@@ -43,7 +45,59 @@ sed -i '/production = true/d' -- '.npmrc'
 actions_PATH="$PATH"
 export PATH="$(pwd)/bin/linux/x64/node/node-v$node_version-linux-x64/bin:$PATH"
 bin/linux/x64/node/node-v$node_version-linux-x64/bin/node node_modules/better-npm-audit/index.js audit
-rm bin/linux/x64/node/node-v$node_version-linux-x64/bin/node
 export PATH="$actions_PATH"
+rm -rf .npm/
+rm -r node_modules/
+rm package-lock.json
+git checkout -- '.npmrc' 'package.json'
+# npm latest
+# Ignore dependencies, devDependencies, optionalDependencies, peerDependencies, and bundleDependencies
+sed -i '/"dependencies": {/,/}/d' -- 'package.json'
+sed -i '/"devDependencies": {/,/}/d' -- 'package.json'
+sed -i '/"optionalDependencies": {/,/}/d' -- 'package.json'
+sed -i '/"peerDependencies": {/,/}/d' -- 'package.json'
+sed -i -z 's|  "bundleDependencies": \[\n    ".*"\n  \]|  "bundleDependencies": \[\]|' -- 'package.json'
+# Install
+bin/linux/x64/node/node-v$node_version-linux-x64/bin/node bin/windows/x64/node/node-v$node_version-win-x64/node_modules/npm/bin/npm-cli.js install --no-offline "npm@$new_npm_version"
+rm -rf .npm/
+# Dedupe
+bin/linux/x64/node/node-v$node_version-linux-x64/bin/node bin/windows/x64/node/node-v$node_version-win-x64/node_modules/npm/bin/npm-cli.js dedupe
+rm -rf .npm/
+# Audit
+sed -i '/cache-max = 0/d' -- '.npmrc'
+sed -i '/only = prod/d' -- '.npmrc'
+sed -i '/optional = false/d' -- '.npmrc'
+sed -i '/production = true/d' -- '.npmrc'
+actions_PATH="$PATH"
+export PATH="$(pwd)/bin/linux/x64/node/node-v$node_version-linux-x64/bin:$PATH"
+bin/linux/x64/node/node-v$node_version-linux-x64/bin/node node_modules/better-npm-audit/index.js audit
+export PATH="$actions_PATH"
+rm -rf .npm/
+rm -r node_modules/
+rm package-lock.json
+git checkout -- '.npmrc' 'package.json'
+# npm 6
+# Ignore dependencies, devDependencies, optionalDependencies, peerDependencies, and bundleDependencies
+sed -i '/"dependencies": {/,/}/d' -- 'package.json'
+sed -i '/"devDependencies": {/,/}/d' -- 'package.json'
+sed -i '/"optionalDependencies": {/,/}/d' -- 'package.json'
+sed -i '/"peerDependencies": {/,/}/d' -- 'package.json'
+sed -i -z 's|  "bundleDependencies": \[\n    ".*"\n  \]|  "bundleDependencies": \[\]|' -- 'package.json'
+# Install
+bin/linux/x64/node/node-v$node_version-linux-x64/bin/node bin/windows/x64/node/node-v$node_version-win-x64/node_modules/npm/bin/npm-cli.js install --no-offline "npm@$npm_version"
+rm -rf .npm/
+# Dedupe
+bin/linux/x64/node/node-v$node_version-linux-x64/bin/node bin/windows/x64/node/node-v$node_version-win-x64/node_modules/npm/bin/npm-cli.js dedupe
+rm -rf .npm/
+# Audit
+sed -i '/cache-max = 0/d' -- '.npmrc'
+sed -i '/only = prod/d' -- '.npmrc'
+sed -i '/optional = false/d' -- '.npmrc'
+sed -i '/production = true/d' -- '.npmrc'
+actions_PATH="$PATH"
+export PATH="$(pwd)/bin/linux/x64/node/node-v$node_version-linux-x64/bin:$PATH"
+bin/linux/x64/node/node-v$node_version-linux-x64/bin/node node_modules/better-npm-audit/index.js audit
+export PATH="$actions_PATH"
+rm bin/linux/x64/node/node-v$node_version-linux-x64/bin/node
 rm -rf .npm/
 git checkout -- '.npmrc' 'package.json'
