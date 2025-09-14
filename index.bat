04290327830e82f5ec5abc2b7450c6b48138c14c
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2025-09-14 11:36:36 +0200

    Update index.bat

--- index.bat
+++ index.bat
@@ -23,3 +23,4 @@ for /f "tokens=1,2 delims=:, " %%a in (' find ":" ^< "package.json" ') do (
   set "%%~a_version=%%~b"
 )
 cmd /c bin\windows\x64\node\node-v%node_version%-win-x64\node --use_strict index.js
+pause
