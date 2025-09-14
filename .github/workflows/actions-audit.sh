df8a3b7d7de3e1735fc2f01c6b582df5d042a831
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2025-09-14 09:25:54 +0200

    Reduce down to one install call

--- .github/workflows/actions-audit.sh
+++ .github/workflows/actions-audit.sh
@@ -21,7 +21,6 @@ rm -rf .npm/
 rm -r node_modules/
 rm package-lock.json
 git checkout -- '.npmrc'
-# other dependencies
 better_npm_audit_version=$(cat package.json | python -c "import sys, json; print(json.load(sys.stdin)['dependencies']['better-npm-audit'])")
 electron_version=$(cat package.json | python -c "import sys, json; print(json.load(sys.stdin)['devDependencies']['electron'])")
 lodash_isarray_version=$(cat package.json | python -c "import sys, json; print(json.load(sys.stdin)['optionalDependencies']['lodash.isarray'])")
@@ -32,59 +31,7 @@ sed -i '/"optionalDependencies": {/,/}/d' -- 'package.json'
 sed -i '/"peerDependencies": {/,/}/d' -- 'package.json'
 sed -i -z 's|  "bundleDependencies": \[\n    ".*"\n  \]|  "bundleDependencies": \[\]|' -- 'package.json'
 # Install
-bin/linux/x64/node/node-v$node_version-linux-x64/bin/node bin/windows/x64/node/node-v$node_version-win-x64/node_modules/npm/bin/npm-cli.js install --no-offline "electron@$electron_version" "lodash.isarray@$lodash_isarray_version" "better-npm-audit@$better_npm_audit_version"
-rm -rf .npm/
-# Dedupe
-bin/linux/x64/node/node-v$node_version-linux-x64/bin/node bin/windows/x64/node/node-v$node_version-win-x64/node_modules/npm/bin/npm-cli.js dedupe
-rm -rf .npm/
-# Audit
-sed -i '/cache-max = 0/d' -- '.npmrc'
-sed -i '/only = prod/d' -- '.npmrc'
-sed -i '/optional = false/d' -- '.npmrc'
-sed -i '/production = true/d' -- '.npmrc'
-actions_PATH="$PATH"
-export PATH="$(pwd)/bin/linux/x64/node/node-v$node_version-linux-x64/bin:$PATH"
-bin/linux/x64/node/node-v$node_version-linux-x64/bin/node node_modules/better-npm-audit/index.js audit
-export PATH="$actions_PATH"
-rm -rf .npm/
-rm -r node_modules/
-rm package-lock.json
-git checkout -- '.npmrc' 'package.json'
-# npm latest
-# Ignore dependencies, devDependencies, optionalDependencies, peerDependencies, and bundleDependencies
-sed -i '/"dependencies": {/,/}/d' -- 'package.json'
-sed -i '/"devDependencies": {/,/}/d' -- 'package.json'
-sed -i '/"optionalDependencies": {/,/}/d' -- 'package.json'
-sed -i '/"peerDependencies": {/,/}/d' -- 'package.json'
-sed -i -z 's|  "bundleDependencies": \[\n    ".*"\n  \]|  "bundleDependencies": \[\]|' -- 'package.json'
-# Install
-bin/linux/x64/node/node-v$node_version-linux-x64/bin/node bin/windows/x64/node/node-v$node_version-win-x64/node_modules/npm/bin/npm-cli.js install --no-offline "npm@$new_npm_version" "better-npm-audit@$better_npm_audit_version"
-rm -rf .npm/
-# Dedupe
-bin/linux/x64/node/node-v$node_version-linux-x64/bin/node bin/windows/x64/node/node-v$node_version-win-x64/node_modules/npm/bin/npm-cli.js dedupe
-rm -rf .npm/
-# Audit
-sed -i '/cache-max = 0/d' -- '.npmrc'
-sed -i '/only = prod/d' -- '.npmrc'
-sed -i '/optional = false/d' -- '.npmrc'
-sed -i '/production = true/d' -- '.npmrc'
-actions_PATH="$PATH"
-export PATH="$(pwd)/bin/linux/x64/node/node-v$node_version-linux-x64/bin:$PATH"
-bin/linux/x64/node/node-v$node_version-linux-x64/bin/node node_modules/better-npm-audit/index.js audit
-export PATH="$actions_PATH"
-rm -rf .npm/
-rm -r node_modules/
-rm package-lock.json
-git checkout -- '.npmrc' 'package.json'
-# npm 6
-# Ignore dependencies, devDependencies, optionalDependencies, peerDependencies, and bundleDependencies
-sed -i '/"dependencies": {/,/}/d' -- 'package.json'
-sed -i '/"devDependencies": {/,/}/d' -- 'package.json'
-sed -i '/"optionalDependencies": {/,/}/d' -- 'package.json'
-sed -i '/"peerDependencies": {/,/}/d' -- 'package.json'
-sed -i -z 's|  "bundleDependencies": \[\n    ".*"\n  \]|  "bundleDependencies": \[\]|' -- 'package.json'
-# Install
-bin/linux/x64/node/node-v$node_version-linux-x64/bin/node bin/windows/x64/node/node-v$node_version-win-x64/node_modules/npm/bin/npm-cli.js install --no-offline "npm@$npm_version" "better-npm-audit@$better_npm_audit_version"
+bin/linux/x64/node/node-v$node_version-linux-x64/bin/node bin/windows/x64/node/node-v$node_version-win-x64/node_modules/npm/bin/npm-cli.js install --no-offline "electron@$electron_version" "lodash.isarray@$lodash_isarray_version" "npm@$new_npm_version" "npm-6@npm:npm@$npm_version" "better-npm-audit@$better_npm_audit_version"
 rm -rf .npm/
 # Dedupe
 bin/linux/x64/node/node-v$node_version-linux-x64/bin/node bin/windows/x64/node/node-v$node_version-win-x64/node_modules/npm/bin/npm-cli.js dedupe
@@ -100,4 +47,6 @@ bin/linux/x64/node/node-v$node_version-linux-x64/bin/node node_modules/better-np
 export PATH="$actions_PATH"
 rm bin/linux/x64/node/node-v$node_version-linux-x64/bin/node
 rm -rf .npm/
+rm -r node_modules/
+rm package-lock.json
 git checkout -- '.npmrc' 'package.json'
