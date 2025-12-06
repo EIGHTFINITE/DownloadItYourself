fcd11da1b5762d07a994c789914a772a341c948a
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2025-12-06 17:24:49 +0100

    Update actions-clean-files.sh

--- .github/workflows/actions-clean-files.sh
+++ .github/workflows/actions-clean-files.sh
@@ -3,6 +3,7 @@
 find node_modules/ -mindepth 2 -type d \( -name '.github' -o -name 'benchmark' -o -name 'docs' -o -name 'example' -o -name 'examples' -o -name 'jsdoc-toolkit' -o -name 'man' -o -name 'tap-snapshots' -o -name 'test' -o -name 'tests' -o -name 'typings' \) | xargs rm -rf
 find node_modules/ -mindepth 2 -type f \( -name '*.d.ts' -o -name '*.d.ts.map' -o -name '*.js.map' -o -name '.editorconfig' -o -name '.eslintignore' -o -name '.eslintrc' -o -name '.eslintrc.json' -o -name '.eslintrc.yml' -o -name '.gitattributes' -o -name '.gitmodules' -o -name '.licensee.json' -o -name '.mailmap' -o -name '.npmignore' -o -name '.nycrc' -o -name '.project' -o -name '.runkit_example.js' -o -name '.travis.yml' -o -name 'configure' -o -name 'Jenkinsfile' -o -name 'make.bat' -o -name 'Makefile' -o -name 'tsconfig.json' -o -name 'yarn.lock' \) -exec bash -c 'rm "$1"; rmdir --ignore-fail-on-non-empty $(dirname "$1")' bash '{}' ';'
 rm -rf 'node_modules/electron-chrome-extensions/dist/types'
+rm -rf 'node_modules/electron-chrome-web-store/dist/types'
 # Remove non-deterministic information
 find node_modules/ -mindepth 2 -type f -name 'package.json' -exec sed -i '/"_where": "/d' -- '{}' ';'
 find node_modules/ -mindepth 2 -type f -name 'package.json' -exec sed -i '/"man": \[/,/\],/d' -- '{}' ';'
