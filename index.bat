b1fee7e10e085c0d41f105b048ec4d19041a6729
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2025-12-17 17:29:13 +0100

    Download git if it is missing

--- index.bat
+++ index.bat
@@ -1,30 +1,52 @@
 @echo off
+:: https://learn.microsoft.com/en-us/windows/win32/intl/code-page-identifiers
+chcp 437 >nul
 cd /d "%~dp0"
-git init .
-git config core.autocrlf false
-git config core.ignorecase false
-git config core.fscache true
-git config core.longpaths true
-git config diff.renameLimit 0
-git config status.renameLimit 0
-git config merge.renameLimit 0
-git config http.lowSpeedLimit 0
-git config http.lowSpeedTime 300
-git config http.postBuffer 1048576000
-git config pack.threads 1
-git config index.threads 0
-git remote add origin https://github.com/EIGHTFINITE/DownloadItYourself.git 2>nul
-git remote set-url origin https://github.com/EIGHTFINITE/DownloadItYourself.git
-git fetch --force --all --tags
-git reset --hard
-git checkout -B master refs/remotes/origin/master
-git reflog expire --expire-unreachable=now --all
-git gc --aggressive --prune=all
-git fsck --unreachable --no-reflogs
-for /f "tokens=1,2 delims=:, " %%a in (' find ":" ^< "package.json" ') do (
-  set "%%~a_version=%%~b"
+goto:start
+
+:git
+if not exist "bin\windows\x64\git\PortableGit-%portable-git-short-version%-64-bit\bin\git.exe" (
+  echo Missing git. Downloading now...
+  curl.exe -#Lo"artifacts.zip" https://github.com/EIGHTFINITE/DownloadItYourself/archive/refs/tags/artifacts.zip
+  echo Extracting git...
+  tar.exe -xf "artifacts.zip" --strip-components=1 -C "."
+  del artifacts.zip
+  for /f "tokens=1,2 delims=:, " %%a in (' find ":" ^< "downloadlist.json" ') do (
+    set "%%~a-version=%%~b"
+  )
 )
-git checkout refs/tags/artifacts -- "bin/linux/x64/node/node-v%node_version%-linux-x64/lib/" bin/windows/ extensions/ node_modules/ package-lock.json
-git clean -ffxe "bin/windows/x64/electron/electron-v%electron_version%-win32-x64/electron.exe" -- bin/ extensions/ node_modules/
-git reset -- "bin/linux/x64/node/node-v%node_version%-linux-x64/lib/" bin/windows/ extensions/ node_modules/ package-lock.json
-cmd /c "bin\windows\x64\node\node-v%node_version%-win-x64\node" --use_strict index.js
+bin\windows\x64\git\PortableGit-%portable-git-short-version%-64-bit\bin\git.exe %*
+goto:eof
+
+:start
+for /f "tokens=1,2 delims=:, " %%a in (' find ":" ^< "downloadlist.json" ') do (
+  set "%%~a-version=%%~b"
+)
+call:git init .
+call:git config core.autocrlf false
+call:git config core.ignorecase false
+call:git config core.fscache true
+call:git config core.longpaths true
+call:git config diff.renameLimit 0
+call:git config status.renameLimit 0
+call:git config merge.renameLimit 0
+call:git config http.lowSpeedLimit 0
+call:git config http.lowSpeedTime 300
+call:git config http.postBuffer 1048576000
+call:git config pack.threads 1
+call:git config index.threads 0
+call:git remote add origin https://github.com/EIGHTFINITE/DownloadItYourself.call:git 2>nul
+call:git remote set-url origin https://github.com/EIGHTFINITE/DownloadItYourself.git
+call:git fetch --force --all --tags
+call:git reset --hard
+call:git checkout -B master refs/remotes/origin/master
+call:git reflog expire --expire-unreachable=now --all
+call:git gc --aggressive --prune=all
+call:git fsck --unreachable --no-reflogs
+for /f "tokens=1,2 delims=:, " %%a in (' find ":" ^< "package.json" ') do (
+  set "%%~a-version=%%~b"
+)
+call:git checkout refs/tags/artifacts -- "bin/linux/x64/node/node-v%node-version%-linux-x64/lib/" bin/windows/ extensions/ node_modules/ package-lock.json
+call:git clean -ffxe "bin/windows/x64/electron/electron-v%electron-version%-win32-x64/electron.exe" -- bin/ extensions/ node_modules/
+call:git reset -- "bin/linux/x64/node/node-v%node-version%-linux-x64/lib/" bin/windows/ extensions/ node_modules/ package-lock.json
+cmd /c "bin\windows\x64\node\node-v%node-version%-win-x64\node" --use_strict index.js
